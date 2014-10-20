4479cdd33fabc07731a7a53327e46b88eb0c4bc6
maestradaz3 <maestradaz3@maestradaz3-PC>
2014-10-19 20:38:26 -0500

    Facturas

--- src/Brasa/LogisticaBundle/Entity/LogFacturas.php
+++ src/Brasa/LogisticaBundle/Entity/LogFacturas.php
@@ -28,7 +28,36 @@ class LogFacturas
      */    
     private $fecha;  
     
+    /**
+     * @ORM\Column(name="fecha_vencimiento", type="datetime", nullable=true)
+     */    
+    private $fechaVencimiento;    
+    
+    /**
+     * @ORM\Column(name="ct_unidades", type="float")
+     */
+    private $ctUnidades = 0;    
+    
+    /**
+     * @ORM\Column(name="vr_flete", type="float")
+     */
+    private $vrFlete = 0;    
+    
+    /**
+     * @ORM\Column(name="vr_manejo", type="float")
+     */
+    private $vrManejo = 0;    
 
+    /**
+     * @ORM\Column(name="vr_otros", type="float")
+     */
+    private $vrOtros = 0;    
+    
+    /**
+     * @ORM\Column(name="comentarios", type="string", length=500, nullable=true)
+     */    
+    private $comentarios;     
+    
     /**
      * Get codigoFacturaPk
      *
@@ -83,5 +112,143 @@ class LogFacturas
     public function getFecha()
     {
         return $this->fecha;
+    }
+
+    /**
+     * Set fechaVencimiento
+     *
+     * @param \DateTime $fechaVencimiento
+     * @return LogFacturas
+     */
+    public function setFechaVencimiento($fechaVencimiento)
+    {
+        $this->fechaVencimiento = $fechaVencimiento;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaVencimiento
+     *
+     * @return \DateTime 
+     */
+    public function getFechaVencimiento()
+    {
+        return $this->fechaVencimiento;
+    }
+
+    /**
+     * Set ctUnidades
+     *
+     * @param float $ctUnidades
+     * @return LogFacturas
+     */
+    public function setCtUnidades($ctUnidades)
+    {
+        $this->ctUnidades = $ctUnidades;
+
+        return $this;
+    }
+
+    /**
+     * Get ctUnidades
+     *
+     * @return float 
+     */
+    public function getCtUnidades()
+    {
+        return $this->ctUnidades;
+    }
+
+    /**
+     * Set vrFlete
+     *
+     * @param float $vrFlete
+     * @return LogFacturas
+     */
+    public function setVrFlete($vrFlete)
+    {
+        $this->vrFlete = $vrFlete;
+
+        return $this;
+    }
+
+    /**
+     * Get vrFlete
+     *
+     * @return float 
+     */
+    public function getVrFlete()
+    {
+        return $this->vrFlete;
+    }
+
+    /**
+     * Set vrManejo
+     *
+     * @param float $vrManejo
+     * @return LogFacturas
+     */
+    public function setVrManejo($vrManejo)
+    {
+        $this->vrManejo = $vrManejo;
+
+        return $this;
+    }
+
+    /**
+     * Get vrManejo
+     *
+     * @return float 
+     */
+    public function getVrManejo()
+    {
+        return $this->vrManejo;
+    }
+
+    /**
+     * Set vrOtros
+     *
+     * @param float $vrOtros
+     * @return LogFacturas
+     */
+    public function setVrOtros($vrOtros)
+    {
+        $this->vrOtros = $vrOtros;
+
+        return $this;
+    }
+
+    /**
+     * Get vrOtros
+     *
+     * @return float 
+     */
+    public function getVrOtros()
+    {
+        return $this->vrOtros;
+    }
+
+    /**
+     * Set comentarios
+     *
+     * @param string $comentarios
+     * @return LogFacturas
+     */
+    public function setComentarios($comentarios)
+    {
+        $this->comentarios = $comentarios;
+
+        return $this;
+    }
+
+    /**
+     * Get comentarios
+     *
+     * @return string 
+     */
+    public function getComentarios()
+    {
+        return $this->comentarios;
     }
 }
