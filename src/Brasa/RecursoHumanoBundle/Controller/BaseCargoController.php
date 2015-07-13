cce3c634b98e2e5dd4759134f0fb8d1b854dced1
coordsistemas <coordsistemas@pc03.JGEFECTIVOS.LOCAL>
2015-07-13 08:45:55 -0500

    crud cargo

---
+++ src/Brasa/RecursoHumanoBundle/Controller/BaseCargoController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Brasa\RecursoHumanoBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Brasa\RecursoHumanoBundle\Form\Type\RhuCargoType;
+
+/**
+ * RhuEntidadSalud controller.
+ *
+ */
+class BaseCargoController extends Controller
+{
+
+    public function listarAction() {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest(); // captura o recupera datos del formulario
+        $paginator  = $this->get('knp_paginator');
+        $form = $this->createFormBuilder() //
+            ->add('BtnPdf', 'submit', array('label'  => 'PDF'))
+            ->add('BtnExcel', 'submit', array('label'  => 'Excel'))
+            ->add('BtnEliminar', 'submit', array('label'  => 'Eliminar'))
+            ->getForm(); 
+        $form->handleRequest($request);
+        $arCargos = new \Brasa\RecursoHumanoBundle\Entity\RhuCargo();
+        if($form->isValid()) {
+            $arrSeleccionados = $request->request->get('ChkSeleccionar');
+            if(count($arrSeleccionados) > 0) {
+                foreach ($arrSeleccionados AS $codigoCargoPk) {
+                    $arCargo = new \Brasa\RecursoHumanoBundle\Entity\RhuCargo();
+                    $arCargo = $em->getRepository('BrasaRecursoHumanoBundle:RhuCargo')->find($codigoCargoPk);
+                    $em->remove($arCargo);
+                    $em->flush();
+                }
+            }
+            
+        if($form->get('BtnPdf')->isClicked()) {
+                $objFormatoCargo = new \Brasa\RecursoHumanoBundle\Formatos\FormatoCargo();
+                $objFormatoCargo->Generar($this);
+        }    
+        
+        if($form->get('BtnExcel')->isClicked()) {
+                $objPHPExcel = new \PHPExcel();
+                // Set document properties
+                $objPHPExcel->getProperties()->setCreator("JG Efectivos")
+                    ->setLastModifiedBy("JG Efectivos")
+                    ->setTitle("Office 2007 XLSX Test Document")
+                    ->setSubject("Office 2007 XLSX Test Document")
+                    ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+                    ->setKeywords("office 2007 openxml php")
+                    ->setCategory("Test result file");
+
+                $objPHPExcel->setActiveSheetIndex(0)
+                            ->setCellValue('A1', 'Codigo')
+                            ->setCellValue('B1', 'Nombre');
+
+                $i = 2;
+                $arCargos = $em->getRepository('BrasaRecursoHumanoBundle:RhuCargo')->findAll();
+                
+                foreach ($arCargos as $arCargos) {
+                    $objPHPExcel->setActiveSheetIndex(0)
+                            ->setCellValue('A' . $i, $arCargos->getCodigoCargoPk())
+                            ->setCellValue('B' . $i, $arCargos->getNombre());
+                    $i++;
+                }
+
+                $objPHPExcel->getActiveSheet()->setTitle('Cargos');
+                $objPHPExcel->setActiveSheetIndex(0);
+
+                // Redirect output to a client’s web browser (Excel2007)
+                header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                header('Content-Disposition: attachment;filename="Cargos.xlsx"');
+                header('Cache-Control: max-age=0');
+                // If you're serving to IE 9, then the following may be needed
+                header('Cache-Control: max-age=1');
+                // If you're serving to IE over SSL, then the following may be needed
+                header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+                header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+                header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+                header ('Pragma: public'); // HTTP/1.0
+                $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
+                $objWriter->save('php://output');
+                exit;
+            }
+            
+        }
+        $arCargos = new \Brasa\RecursoHumanoBundle\Entity\RhuCargo();
+        $query = $em->getRepository('BrasaRecursoHumanoBundle:RhuCargo')->findAll();
+        $arCargos = $paginator->paginate($query, $this->get('request')->query->get('page', 1),20);
+
+        return $this->render('BrasaRecursoHumanoBundle:Base/Cargo:listar.html.twig', array(
+                    'arCargos' => $arCargos,
+                    'form'=> $form->createView()
+           
+        ));
+    }
+    public function nuevoAction($codigoCargoPk) {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
+        $arCargo = new \Brasa\RecursoHumanoBundle\Entity\RhuCargo();
+        if ($codigoCargoPk != 0)
+        {
+            $arCargo = $em->getRepository('BrasaRecursoHumanoBundle:RhuCargo')->find($codigoCargoPk);
+        }    
+        $formCargo = $this->createForm(new RhuCargoType(), $arCargo);
+        $formCargo->handleRequest($request);
+        if ($formCargo->isValid())
+        {
+            // guardar la tarea en la base de datos
+            $em->persist($arCargo);
+            $arCargo = $formCargo->getData();
+            $em->flush();
+            return $this->redirect($this->generateUrl('brs_rhu_base_cargo_listar'));
+        }
+        return $this->render('BrasaRecursoHumanoBundle:Base/Cargo:nuevo.html.twig', array(
+            'formCargo' => $formCargo->createView(),
+        ));
+    }
+    
+}
---
+++ src/Brasa/RecursoHumanoBundle/Formatos/FormatoCargo.php
@@ -0,0 +1,68 @@
+<?php
+namespace Brasa\RecursoHumanoBundle\Formatos;
+class FormatoCargo extends \FPDF_FPDF {
+    public static $em;
+   
+    public function Generar($miThis) {        
+        ob_clean();
+        $em = $miThis->getDoctrine()->getManager();
+        self::$em = $em;
+        $pdf = new FormatoCargo();
+        $pdf->AliasNbPages();
+        $pdf->AddPage();
+        $pdf->SetFont('Arial', '', 12);
+        $this->Body($pdf);
+        $pdf->Output("Lista_cargos.pdf", 'D');        
+        
+    } 
+    
+    public function Header() {
+        $this->SetFillColor(236, 236, 236);        
+        $this->SetFont('Arial','B',12);
+        //$this->Image('imagenes/logos/LogoCotrascal.jpg', 10, 10, 35, 17);        
+        $this->SetXY(10, 20);
+        $this->Cell(190, 8, "LISTADO CARGOS" , 1, 0, 'C', 1);
+        $this->EncabezadoDetalles();
+        
+    }
+
+    public function EncabezadoDetalles() {
+        $this->Ln(8);
+        $header = array('CODIGO', 'NOMBRE');
+        $this->SetFillColor(236, 236, 236);
+        $this->SetTextColor(0);
+        $this->SetDrawColor(0, 0, 0);
+        $this->SetLineWidth(.2);
+        $this->SetFont('Arial', 'B', 8);
+        //creamos la cabecera de la tabla.
+        $w = array(20, 170);
+        for ($i = 0; $i < count($header); $i++)
+            if ($i == 0 || $i == 1)
+                $this->Cell($w[$i], 4, $header[$i], 1, 0, 'L', 1);
+            else
+                $this->Cell($w[$i], 4, $header[$i], 1, 0, 'C', 1);
+        //Restauracion de colores y fuentes
+        $this->SetFillColor(224, 235, 255);
+        $this->SetTextColor(0);
+        $this->SetFont('');
+        $this->Ln(4);
+    }
+
+    public function Body($pdf) {
+        $arCargos = new \Brasa\RecursoHumanoBundle\Entity\RhuCargo();
+        $arCargos = self::$em->getRepository('BrasaRecursoHumanoBundle:RhuCargo')->findAll();
+        $pdf->SetX(10);
+        $pdf->SetFont('Arial', '', 8);
+        foreach ($arCargos as $arCargos) {            
+            $pdf->Cell(20, 4, $arCargos->getCodigoCargoPk(), 1, 0, 'L');
+            $pdf->Cell(170, 4, utf8_decode($arCargos->getNombre()), 1, 0, 'L');            
+            $pdf->Ln();
+            $pdf->SetAutoPageBreak(true, 15);//33
+        }        
+    }
+
+    public function Footer() {
+        $this->SetXY(160, 270);
+        $this->Cell(30, 35, utf8_decode('JG Efectivos S.A.S.   Página ') . $this->PageNo() . ' de {nb}' , 0, 0, 'L', 0);          
+    }    
+}
