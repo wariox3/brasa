57320bb8e7e07fa213c28a0340e38d587761a0d8
jefedesarrollo <jefedesarrollo@jefedesarrollo-H81M-H>
2015-09-07 07:52:39 -0500

    Entidades

--- src/Brasa/GeneralBundle/Controller/DefaultController.php
+++ src/Brasa/GeneralBundle/Controller/DefaultController.php
@@ -27,15 +27,15 @@ class DefaultController extends Controller
     
     public function menuAction()
     {
-        $arUsuario = new \Brasa\SeguridadBundle\Entity\User();
-        $arUsuario = $this->get('security.context')->getToken()->getUser();
-        $strUsuario = $arUsuario->getNombreCorto();
+        //$arUsuario = new \Brasa\SeguridadBundle\Entity\User();
+        //$arUsuario = $this->get('security.context')->getToken()->getUser();
+        //$strUsuario = $arUsuario->getNombreCorto();
         //$destinatario = $this->contenedor->getParameter('contact_email');
         //$obj = new \Brasa\GeneralBundle\MisClases\CambiarBD();
         //$obj->setUpAppConnection($this);
         //\Brasa\GeneralBundle\MisClases\CambiarBD::setUpAppConnection();
         
-        return $this->render('BrasaGeneralBundle:plantillas:menu.html.twig', array('Usuario' => $strUsuario));
+        return $this->render('BrasaGeneralBundle:plantillas:menu.html.twig');
     }                  
     
 }
