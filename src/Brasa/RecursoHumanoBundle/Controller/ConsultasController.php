efaca35fb83bc083f6674f63c30c54dd62261557
coordsistemas <coordsistemas@pc03.JGEFECTIVOS.LOCAL>
2015-09-19 08:26:39 -0500

    excel consulta costos ibc

--- src/Brasa/RecursoHumanoBundle/Controller/ConsultasController.php
+++ src/Brasa/RecursoHumanoBundle/Controller/ConsultasController.php
@@ -62,7 +62,7 @@ class ConsultasController extends Controller
             if($form->get('BtnExcelCostosIbc')->isClicked()) {
                 $this->filtrarCostosIbcLista($form);
                 $this->listarCostosIbc();
-                $this->generarExcelCostosIbc();
+                $this->generarCostosIbcExcel();
             }            
             if($form->get('BtnFiltrarCostosIbc')->isClicked()) {
                 $this->filtrarCostosIbcLista($form);
@@ -788,75 +788,35 @@ class ConsultasController extends Controller
 
         $objPHPExcel->setActiveSheetIndex(0)
                     ->setCellValue('A1', 'CODIGO')
-                    ->setCellValue('B1', 'DESDE')
-                    ->setCellValue('C1', 'HASTA')
-                    ->setCellValue('D1', 'IDENTIFICACION')
-                    ->setCellValue('E1', 'NOMBRE')
-                    ->setCellValue('F1', 'CENTRO COSTOS')
-                    ->setCellValue('G1', 'BASICO')
-                    ->setCellValue('H1', 'TIEMPO EXTRA')
-                    ->setCellValue('I1', 'VALORES ADICIONALES')
-                    ->setCellValue('J1', 'AUX. TRANSPORTE')
-                    ->setCellValue('K1', 'ARP')
-                    ->setCellValue('L1', 'EPS')
-                    ->setCellValue('M1', 'PENSION')
-                    ->setCellValue('N1', 'CAJA')
-                    ->setCellValue('O1', 'ICBF')
-                    ->setCellValue('P1', 'SENA')
-                    ->setCellValue('Q1', 'CESANTIAS')
-                    ->setCellValue('R1', 'VACACIONES')
-                    ->setCellValue('S1', 'ADMON')
-                    ->setCellValue('T1', 'COSTO')
-                    ->setCellValue('U1', 'TOTAL')
-                    ->setCellValue('W1', 'NETO')
-                    ->setCellValue('X1', 'IBC')
-                    ->setCellValue('Y1', 'AUX. TRANSPORTE COTIZACION')
-                    ->setCellValue('Z1', 'DIAS PERIODO')
-                    ->setCellValue('AA1', 'SALARIO PERIODO')
-                    ->setCellValue('AB1', 'SALARIO EMPLEADO');
+                    ->setCellValue('B1', 'IDENTIFICACION')
+                    ->setCellValue('C1', 'EMPLEADO')
+                    ->setCellValue('D1', 'CODIGO CONTRATO')
+                    ->setCellValue('E1', 'IBC')
+                    ->setCellValue('F1', 'DESDE')
+                    ->setCellValue('G1', 'HASTA');
 
         $i = 2;
-        $query = $em->createQuery($this->strSqlLista);
-        $arPagos = new \Brasa\RecursoHumanoBundle\Entity\RhuPago();
-        $arPagos = $query->getResult();
-        foreach ($arPagos as $arPago) {
+        $query = $em->createQuery($this->strSqlCostosIbcLista);
+        $arIbc = new \Brasa\RecursoHumanoBundle\Entity\RhuIbc();
+        $arIbc = $query->getResult();
+        foreach ($arIbc as $arIbc) {
             $objPHPExcel->setActiveSheetIndex(0)
-                    ->setCellValue('A' . $i, $arPago->getCodigoPagoPk())
-                    ->setCellValue('B' . $i, $arPago->getFechaDesde()->Format('Y-m-d'))
-                    ->setCellValue('C' . $i, $arPago->getFechaHasta()->Format('Y-m-d'))
-                    ->setCellValue('D' . $i, $arPago->getEmpleadoRel()->getNumeroIdentificacion())
-                    ->setCellValue('E' . $i, $arPago->getEmpleadoRel()->getNombreCorto())
-                    ->setCellValue('F' . $i, $arPago->getCentroCostoRel()->getNombre())
-                    ->setCellValue('G' . $i, $arPago->getVrSalario())
-                    ->setCellValue('H' . $i, $arPago->getVrAdicionalTiempo())
-                    ->setCellValue('I' . $i, $arPago->getVrAdicionalValor())
-                    ->setCellValue('J' . $i, $arPago->getVrAuxilioTransporte())
-                    ->setCellValue('K' . $i, $arPago->getVrArp())
-                    ->setCellValue('L' . $i, $arPago->getVrEps())
-                    ->setCellValue('M' . $i, $arPago->getVrPension())
-                    ->setCellValue('N' . $i, $arPago->getVrCaja())
-                    ->setCellValue('O' . $i, $arPago->getVrIcbf())
-                    ->setCellValue('P' . $i, $arPago->getVrSena())
-                    ->setCellValue('Q' . $i, $arPago->getVrCesantias())
-                    ->setCellValue('R' . $i, $arPago->getVrVacaciones())
-                    ->setCellValue('S' . $i, $arPago->getVrAdministracion())
-                    ->setCellValue('T' . $i, $arPago->getVrCosto())
-                    ->setCellValue('U' . $i, $arPago->getVrTotalCobrar())
-                    ->setCellValue('W' . $i, $arPago->getVrNeto())
-                    ->setCellValue('X' . $i, $arPago->getVrIngresoBaseCotizacion())
-                    ->setCellValue('Y' . $i, $arPago->getVrAuxilioTransporteCotizacion())
-                    ->setCellValue('Z' . $i, $arPago->getDiasPeriodo())
-                    ->setCellValue('AA' . $i, $arPago->getVrSalarioPeriodo())
-                    ->setCellValue('AB' . $i, $arPago->getVrSalarioEmpleado());
+                    ->setCellValue('A' . $i, $arIbc->getCodigoIbcPk())
+                    ->setCellValue('B' . $i, $arIbc->getEmpleadoRel()->getNumeroIdentificacion())
+                    ->setCellValue('C' . $i, $arIbc->getEmpleadoRel()->getNombreCorto())
+                    ->setCellValue('D' . $i, $arIbc->getCodigoContratoFk())
+                    ->setCellValue('E' . $i, $arIbc->getvrIngresoBaseCotizacion())
+                    ->setCellValue('F' . $i, $arIbc->getFechaDesde()->Format('Y-m-d'))
+                    ->setCellValue('G' . $i, $arIbc->getFechaDesde()->Format('Y-m-d'));
             $i++;
         }
 
-        $objPHPExcel->getActiveSheet()->setTitle('costos');
+        $objPHPExcel->getActiveSheet()->setTitle('CostosIbcc');
         $objPHPExcel->setActiveSheetIndex(0);
 
         // Redirect output to a client’s web browser (Excel2007)
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="Costos.xlsx"');
+        header('Content-Disposition: attachment;filename="CostosIbc.xlsx"');
         header('Cache-Control: max-age=0');
         // If you're serving to IE 9, then the following may be needed
         header('Cache-Control: max-age=1');
