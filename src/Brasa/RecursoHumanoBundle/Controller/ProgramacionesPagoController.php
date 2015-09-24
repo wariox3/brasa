4a666ec51f9ef42e2576902d7b5499a54518fba7
coordsistemas <coordsistemas@pc03.JGEFECTIVOS.LOCAL>
2015-09-24 13:33:41 -0500

    boton excel programaciones de pago

--- src/Brasa/RecursoHumanoBundle/Controller/ProgramacionesPagoController.php
+++ src/Brasa/RecursoHumanoBundle/Controller/ProgramacionesPagoController.php
@@ -48,7 +48,94 @@ class ProgramacionesPagoController extends Controller
                 $em->getRepository('BrasaRecursoHumanoBundle:RhuProgramacionPago')->pagar($codigoProgramacionPago);
                 return $this->redirect($this->generateUrl('brs_rhu_programaciones_pago_lista'));
             }            
+            if($request->request->get('OpExcelProgramacionPagoDetalles')) {
+                $codigoProgramacionPago = $request->request->get('OpExcelProgramacionPagoDetalles');                
+                $em = $this->getDoctrine()->getManager();
+                $objPHPExcel = new \PHPExcel();
+                // Set document properties
+                $objPHPExcel->getProperties()->setCreator("EMPRESA")
+                    ->setLastModifiedBy("EMPRESA")
+                    ->setTitle("Office 2007 XLSX Test Document")
+                    ->setSubject("Office 2007 XLSX Test Document")
+                    ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+                    ->setKeywords("office 2007 openxml php")
+                    ->setCategory("Test result file");
 
+                $objPHPExcel->setActiveSheetIndex(0)
+                            ->setCellValue('A1', 'Codigo');
+
+                $i = 2;
+                
+                $arProgramacionesPagos = new \Brasa\RecursoHumanoBundle\Entity\RhuProgramacionPago();
+                $arProgramacionesPagos = $em->getRepository('BrasaRecursoHumanoBundle:RhuProgramacionPago')->find($codigoProgramacionPago);
+                foreach ($arProgramacionesPagos as $arProgramacionesPago) {
+                    $objPHPExcel->setActiveSheetIndex(0)
+                            ->setCellValue('A' . $i, $arProgramacionesPago->getProgramacionPagoPk());
+                    $i++;
+                }
+
+                $objPHPExcel->getActiveSheet()->setTitle('DetalleProgramacionesPagos');
+                $objPHPExcel->setActiveSheetIndex(0);
+
+                // Redirect output to a client’s web browser (Excel2007)
+                header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                header('Content-Disposition: attachment;filename="DetalleProgramacionesPagos.xlsx"');
+                header('Cache-Control: max-age=0');
+                // If you're serving to IE 9, then the following may be needed
+                header('Cache-Control: max-age=1');
+                // If you're serving to IE over SSL, then the following may be needed
+                header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+                header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+                header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+                header ('Pragma: public'); // HTTP/1.0
+                $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
+                $objWriter->save('php://output');
+                exit;
+            }
+            if($request->request->get('OpExcelPagos')) {
+                $codigoProgramacionPago = $request->request->get('OpExcelProgramacionPagoDetalles');                
+                $em = $this->getDoctrine()->getManager();
+                $objPHPExcel = new \PHPExcel();
+                // Set document properties
+                $objPHPExcel->getProperties()->setCreator("EMPRESA")
+                    ->setLastModifiedBy("EMPRESA")
+                    ->setTitle("Office 2007 XLSX Test Document")
+                    ->setSubject("Office 2007 XLSX Test Document")
+                    ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+                    ->setKeywords("office 2007 openxml php")
+                    ->setCategory("Test result file");
+
+                $objPHPExcel->setActiveSheetIndex(0)
+                            ->setCellValue('A1', 'Codigo');
+
+                $i = 2;
+                
+                $arPagoDetalles = new \Brasa\RecursoHumanoBundle\Entity\RhuPago();
+                $arPagoDetalles = $em->getRepository('BrasaRecursoHumanoBundle:RhuPago')->findBy(array('codigoProgramacionPagoFk' => $codigoProgramacionPago));
+                foreach ($arPagoDetalles as $arPagoDetalle) {
+                    $objPHPExcel->setActiveSheetIndex(0)
+                            ->setCellValue('A' . $i, $arPagoDetalle->getPagoPk());
+                    $i++;
+                }
+
+                $objPHPExcel->getActiveSheet()->setTitle('DetallePagos');
+                $objPHPExcel->setActiveSheetIndex(0);
+
+                // Redirect output to a client’s web browser (Excel2007)
+                header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+                header('Content-Disposition: attachment;filename="DetallePagos.xlsx"');
+                header('Cache-Control: max-age=0');
+                // If you're serving to IE 9, then the following may be needed
+                header('Cache-Control: max-age=1');
+                // If you're serving to IE over SSL, then the following may be needed
+                header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+                header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+                header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+                header ('Pragma: public'); // HTTP/1.0
+                $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
+                $objWriter->save('php://output');
+                exit;
+            }
             if($form->get('BtnEliminarPago')->isClicked()) {
                 if ($arrSeleccionados > 0 ){
                     foreach ($arrSeleccionados AS $codigoProgramacionPago) {
