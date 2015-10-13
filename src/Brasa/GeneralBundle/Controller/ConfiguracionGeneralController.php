49407dffa2e4ea17b3e00986421f3c25f3308567
jefedesarrollo <jefedesarrollo@jefedesarrollo-H81M-H>
2015-10-13 09:21:12 -0500

    Adicionales

--- src/Brasa/GeneralBundle/Controller/ConfiguracionGeneralController.php
+++ src/Brasa/GeneralBundle/Controller/ConfiguracionGeneralController.php
@@ -33,50 +33,82 @@ class ConfiguracionGeneralController extends Controller
             ->add('fechaUltimoCierre', 'date', array('data' => $arConfiguracionGeneral->getFechaUltimoCierre(), 'required' => true))
             ->add('nitVentasMostrador', 'text', array('data' => $arConfiguracionGeneral->getNitVentasMostrador(), 'required' => true))    
             ->add('rutaTemporal', 'text', array('data' => $arConfiguracionGeneral->getRutaTemporal(), 'required' => true))    
-            ->add('guardar', 'submit', array('label' => 'Actualizar'))
+            ->add('guardar', 'submit', array('label' => 'Actualizar'))            
             ->getForm();
         $formConfiguracionGeneral->handleRequest($request);
         if ($formConfiguracionGeneral->isValid()) {
-            $controles = $request->request->get('form');                        
-            
-            $NitEmpresa = $formConfiguracionGeneral->get('nitEmpresa')->getData();
-            $NumeroDv = $controles['digitoVerificacion'];
-            $NombreEmpresa = $controles['nombreEmpresa'];
-            $Sigla = $controles['sigla'];
-            $TelefonoEmpresa = $controles['telefonoEmpresa'];
-            $DireccionEmpresa = $controles['direccionEmpresa'];
-            $BaseRetencionFuente = $controles['baseRetencionFuente'];
-            $BaseRetencionCree = $controles['baseRetencionCree'];
-            $PorcentajeRetencionFuente = $controles['porcentajeRetencionFuente'];
-            $PorcentajeRetencionCree = $controles['porcentajeRetencionCree'];
-            $BaseRetencionIvaVentas = $controles['baseRetencionIvaVentas'];
-            $PorcentajeRetencionIvaVentas = $controles['porcentajeRetencionIvaVentas'];
-            $FechaUltimoCierre = $formConfiguracionGeneral->get('fechaUltimoCierre')->getData();
-            $NitVentasMostrador = $controles['nitVentasMostrador'];
-            $RutaTemporal = $controles['rutaTemporal'];
-            // guardar la tarea en la base de datos
-            $arConfiguracionGeneral->setNitEmpresa($NitEmpresa);
-            $arConfiguracionGeneral->setDigitoVerificacionEmpresa($NumeroDv);
-            $arConfiguracionGeneral->setNombreEmpresa($NombreEmpresa);
-            $arConfiguracionGeneral->setSigla($Sigla);
-            $arConfiguracionGeneral->setTelefonoEmpresa($TelefonoEmpresa);
-            $arConfiguracionGeneral->setDireccionEmpresa($DireccionEmpresa);
-            $arConfiguracionGeneral->setBaseRetencionFuente($BaseRetencionFuente);
-            $arConfiguracionGeneral->setBaseRetencionCree($BaseRetencionCree);
-            $arConfiguracionGeneral->setPorcentajeRetencionFuente($PorcentajeRetencionFuente);
-            $arConfiguracionGeneral->setPorcentajeRetencionCREE($PorcentajeRetencionCree);
-            $arConfiguracionGeneral->setBaseRetencionIvaVentas($BaseRetencionIvaVentas);
-            $arConfiguracionGeneral->setPorcentajeRetencionIvaVentas($PorcentajeRetencionIvaVentas);
-            $arConfiguracionGeneral->setFechaUltimoCierre($FechaUltimoCierre);
-            $arConfiguracionGeneral->setNitVentasMostrador($NitVentasMostrador);
-            $arConfiguracionGeneral->setRutaTemporal($RutaTemporal);
-            $em->persist($arConfiguracionGeneral);
-            $em->flush();
-            return $this->redirect($this->generateUrl('brs_gen_configuracion_general', array('codigoConfiguracionPk' => 1)));
+            if($formConfiguracionGeneral->get('guardar')->isClicked()) {
+                $controles = $request->request->get('form');                                    
+                $NitEmpresa = $formConfiguracionGeneral->get('nitEmpresa')->getData();
+                $NumeroDv = $controles['digitoVerificacion'];
+                $NombreEmpresa = $controles['nombreEmpresa'];
+                $Sigla = $controles['sigla'];
+                $TelefonoEmpresa = $controles['telefonoEmpresa'];
+                $DireccionEmpresa = $controles['direccionEmpresa'];
+                $BaseRetencionFuente = $controles['baseRetencionFuente'];
+                $BaseRetencionCree = $controles['baseRetencionCree'];
+                $PorcentajeRetencionFuente = $controles['porcentajeRetencionFuente'];
+                $PorcentajeRetencionCree = $controles['porcentajeRetencionCree'];
+                $BaseRetencionIvaVentas = $controles['baseRetencionIvaVentas'];
+                $PorcentajeRetencionIvaVentas = $controles['porcentajeRetencionIvaVentas'];
+                $FechaUltimoCierre = $formConfiguracionGeneral->get('fechaUltimoCierre')->getData();
+                $NitVentasMostrador = $controles['nitVentasMostrador'];
+                $RutaTemporal = $controles['rutaTemporal'];
+                // guardar la tarea en la base de datos
+                $arConfiguracionGeneral->setNitEmpresa($NitEmpresa);
+                $arConfiguracionGeneral->setDigitoVerificacionEmpresa($NumeroDv);
+                $arConfiguracionGeneral->setNombreEmpresa($NombreEmpresa);
+                $arConfiguracionGeneral->setSigla($Sigla);
+                $arConfiguracionGeneral->setTelefonoEmpresa($TelefonoEmpresa);
+                $arConfiguracionGeneral->setDireccionEmpresa($DireccionEmpresa);
+                $arConfiguracionGeneral->setBaseRetencionFuente($BaseRetencionFuente);
+                $arConfiguracionGeneral->setBaseRetencionCree($BaseRetencionCree);
+                $arConfiguracionGeneral->setPorcentajeRetencionFuente($PorcentajeRetencionFuente);
+                $arConfiguracionGeneral->setPorcentajeRetencionCREE($PorcentajeRetencionCree);
+                $arConfiguracionGeneral->setBaseRetencionIvaVentas($BaseRetencionIvaVentas);
+                $arConfiguracionGeneral->setPorcentajeRetencionIvaVentas($PorcentajeRetencionIvaVentas);
+                $arConfiguracionGeneral->setFechaUltimoCierre($FechaUltimoCierre);
+                $arConfiguracionGeneral->setNitVentasMostrador($NitVentasMostrador);
+                $arConfiguracionGeneral->setRutaTemporal($RutaTemporal);
+                $em->persist($arConfiguracionGeneral);
+                $em->flush();
+                return $this->redirect($this->generateUrl('brs_gen_configuracion_general', array('codigoConfiguracionPk' => 1)));                
+            }
         }
         return $this->render('BrasaGeneralBundle:ConfiguracionGeneral:Configuracion.html.twig', array(
             'formConfiguracionGeneral' => $formConfiguracionGeneral->createView(),
         ));
+    }
+
+    public function borrarBDAction() {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
+        
+        $form = $this->createFormBuilder()            
+            ->add('BtnCargar', 'submit', array('label'  => 'Borrar'))
+            ->getForm();
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            if($form->get('BtnCargar')->isClicked()) {
+                //$form['attachment']->getData()->move("/var/www/temporal", "carga.txt");                            
+                //$fp = fopen("/var/www/temporal/carga.txt", "r");                
+                $fp = fopen("../src/Brasa/GeneralBundle/Resources/sql/limpiarbd.txt", "r");                                
+                $strSql = "";
+                while(!feof($fp)) {
+                    $linea = fgets($fp);
+                    if($linea){
+                        $strSql = $strSql . $linea;
+                    }
+                }
+                fclose($fp);
+                $em->getConnection()->executeQuery($strSql);
+                echo "<script languaje='javascript' type='text/javascript'>window.close();</script>";                
+            }                                   
+        }         
+        return $this->render('BrasaGeneralBundle:ConfiguracionGeneral:borrarBD.html.twig', array(
+            'form' => $form->createView()
+            ));
     }
     
 }
