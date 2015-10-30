2a6403bffd577b3563a335a820f6103210e2adad
coordsistemas <coordsistemas@pc03.JGEFECTIVOS.LOCAL>
2015-10-30 14:00:52 -0500

    desempeño

---
+++ src/Brasa/RecursoHumanoBundle/Controller/DesempenosController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Brasa\RecursoHumanoBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Brasa\RecursoHumanoBundle\Form\Type\RhuDesempenoType;
+use Doctrine\ORM\EntityRepository;
+
+class DesempenosController extends Controller
+{
+    var $strDqlLista = "";
+
+    public function listaAction() {
+        $em = $this->getDoctrine()->getManager();
+        $request = $this->getRequest();
+        $paginator  = $this->get('knp_paginator');
+        $session = $this->getRequest()->getSession();
+        $form = $this->formularioLista();
+        $form->handleRequest($request);
+        $this->listar();
+        if($form->isValid()) {
+            $arrSeleccionados = $request->request->get('ChkSeleccionar');
+            if($form->get('BtnFiltrar')->isClicked()) {
+                $this->filtrar($form);
+                $this->listar();
+            }
+            if($form->get('BtnExcel')->isClicked()) {
+                $this->filtrar($form);
+                $this->listar();
+                $this->generarExcel();
+            }
+
+            if($form->get('BtnEliminar')->isClicked()) {
+                $arrSeleccionados = $request->request->get('ChkSeleccionar');
+                if(count($arrSeleccionados) > 0) {
+                    foreach ($arrSeleccionados AS $codigoDesempeno) {
+                        $arDesempeno = $em->getRepository('BrasaRecursoHumanoBundle:RhuDesempeno')->find($codigoDesempeno);
+                        $em->remove($arDesempeno);
+                        $em->flush();
+                    }
+                }
+            }
+        }
+
+        $arDesempenos = $paginator->paginate($em->createQuery($this->strDqlLista), $request->query->get('page', 1), 20);
+        return $this->render('BrasaRecursoHumanoBundle:Desempenos:lista.html.twig', array(
+            'arDesempenos' => $arDesempenos,
+            'form' => $form->createView()));
+    }
+
+    
+
+}
