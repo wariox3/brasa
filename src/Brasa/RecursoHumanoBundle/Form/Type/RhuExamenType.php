3d24a06f73fe68ad8fe57e76501bfff66e94cf8a
coordsistemas <coordsistemas@pc03-PC.JGEFECTIVOS.LOCAL>
2016-02-06 09:38:57 -0500

    control pago

--- src/Brasa/RecursoHumanoBundle/Form/Type/RhuExamenType.php
+++ src/Brasa/RecursoHumanoBundle/Form/Type/RhuExamenType.php
@@ -38,7 +38,7 @@ class RhuExamenType extends AbstractType
             ->add('identificacion', 'number', array('required' => true))
             ->add('nombreCorto', 'text', array('required' => true))
             ->add('fechaNacimiento','date',array('widget' => 'single_text', 'format' => 'yyyy-MM-dd', 'attr' => array('class' => 'date',)))
-            ->add('pagaEmpleado', 'choice', array('choices'   => array('1' => 'SI', '0' => 'NO')))
+            ->add('controlPago', 'choice', array('choices'   => array('1' => 'SI', '0' => 'NO')))
             ->add('guardar', 'submit')
             ->add('guardarnuevo', 'submit', array('label'  => 'Guardar y Nuevo'));
     }
