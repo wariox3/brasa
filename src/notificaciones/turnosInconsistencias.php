d422c2af25e4553c8d99179464bad6fd62e94a5c
jefedesarrollo <jefedesarrollo@jefedesarrollo-H81M-H>
2016-02-13 10:42:01 -0500

    Actualizacion

--- src/notificaciones/turnosInconsistencias.php
+++ src/notificaciones/turnosInconsistencias.php
@@ -1,4 +1,5 @@
 <?php
+
 ini_set('display_errors', 1);
 include('mail.php');
 include('direcciones.php');
@@ -6,7 +7,13 @@ include('conexion.php');
 $dateFecha = new DateTime('now');
 $strAnio = $dateFecha->format('Y');
 $strMes = $dateFecha->format('m');
+$strUltimoDiaMes = date("d",(mktime(0,0,0,$strMes+1,1,$strAnio)-1));
 
+$strMensaje = "<br />Este es un correo enviado automaticamente desde AppSoga para informarle circunstancias precisas de su interes <br /><br />";
+$strMensaje .= "Modulo: [Turnos] <br />";
+$strMensaje .= "Destinatario: [Jefe operaciones]<br /><br />";
+
+//Recursos sin programaciones
 $arrRecursosSinProgramacion = array();
 $strSql = "SELECT codigo_recurso_pk, nombre_corto "
         . "FROM tur_recurso WHERE estado_activo = 1";
@@ -24,13 +31,9 @@ if ($arRecursos->num_rows > 0) {
         }
     }
 }
-// Armar el cuerpo del mensaje
 if ($arrRecursosSinProgramacion) {
-    $strMensaje = "<br />Este es un correo enviado automaticamente desde sogaapp con el fin de informarle circunstancias precisas de su interes <br /><br />";   
-    $strMensaje .= "Modulo: [Turnos] <br />";    
-    $strMensaje .= "Destinatario: [Jefe operaciones]<br /><br />";    
-    $strMensaje .= "Los siguientes recursos no tienen programacion para el mes $strMes año $strAnio<br /><br />";    
-    $strMensaje .= "<table border='2'>"; 
+    $strMensaje .= "Los siguientes recursos no tienen programacion para el mes $strMes año $strAnio<br /><br />";
+    $strMensaje .= "<table border='2'>";
     $strMensaje .= "<tr>";
     $strMensaje .= "<th>CODIGO</th>";
     $strMensaje .= "<th>NOMBRE</th>";
@@ -41,13 +44,63 @@ if ($arrRecursosSinProgramacion) {
         $strMensaje .= "<td>" . $recurso['nombre'] . "</td>";
         $strMensaje .= "</tr>";
     }
+    $strMensaje .= "</table><br /><br />";
+}
+
+//Turnos dobles
+$arrTurnosDobles = array();
+for ($i = 1; $i <= $strUltimoDiaMes; $i++) {
+    $strSql = "SELECT
+                codigo_recurso_fk as codigo_recurso_fk, 
+                tur_recurso.nombre_corto as nombre_corto,                                                                   
+                COUNT(dia_$i) AS numero
+                FROM
+                tur_programacion_detalle
+                LEFT JOIN tur_recurso ON tur_programacion_detalle.codigo_recurso_fk = tur_recurso.codigo_recurso_pk                                
+                WHERE
+                dia_$i IS NOT NULL AND anio = $strAnio AND mes = $strMes
+                GROUP BY
+                codigo_recurso_fk";
+    $arTurnosDobles = $servidor->query($strSql);
+    if ($arTurnosDobles->num_rows > 0) {
+        while ($arTurnosDoble = $arTurnosDobles->fetch_assoc()) {
+            if($arTurnosDoble['numero'] > 1) {
+                $arrTurnosDobles[] = array(
+                    'codigo' => $arTurnosDoble['codigo_recurso_fk'],
+                    'nombre' => $arTurnosDoble['nombre_corto'],
+                    'dia' => $i
+                );                  
+            }          
+        }
+    }    
+}
+if($arrTurnosDobles) {
+    $strMensaje .= "Los siguientes recursos no tienen doble asignacion de turno para el mes $strMes año $strAnio<br /><br />";
+    $strMensaje .= "<table border='2'>";
+    $strMensaje .= "<tr>";
+    $strMensaje .= "<th>CODIGO</th>";
+    $strMensaje .= "<th>NOMBRE</th>";
+    $strMensaje .= "<th>DIA</th>";
+    $strMensaje .= "</tr>";
+    foreach ($arrTurnosDobles as $recurso) {
+        $strMensaje .= "<tr>";
+        $strMensaje .= "<td>" . $recurso['codigo'] . "</td>";
+        $strMensaje .= "<td>" . $recurso['nombre'] . "</td>";
+        $strMensaje .= "<td>" . $recurso['dia'] . "</td>";
+        $strMensaje .= "</tr>";
+    }
     $strMensaje .= "</table><br /><br />";    
-    $strMensaje .= "Por favor no conteste este mensaje, para comunicarse con servicio al cliente marque 4448120 ext 131<br /><br />";    
+}
+
+//Enviar mensaje
+if ($arrRecursosSinProgramacion || $arrTurnosDobles) {
+    $strMensaje .= "Por favor no conteste este mensaje, para comunicarse con servicio al cliente marque 4448120 ext 131<br /><br />";
     $strMensaje = utf8_decode($strMensaje);
     $arrDirecciones = direcciones('correo_turno_inconsistencia', $servidor);
-    if($arrDirecciones) {
+    if ($arrDirecciones) {
         //echo $strMensaje;
         enviarCorreo($strMensaje, $arrDirecciones,"Inconsistencias recursos [SogaApp-turnos]");         
     }
 }
+
 
