aba692a8506f973578e0c592148f06c3098e5e47
jefedesarrollo <jefedesarrollo@jefedesarrollo-H81M-H>
2016-03-01 15:15:27 -0500

    Exportar banco generar

--- src/Brasa/RecursoHumanoBundle/Controller/PagoBancoController.php
+++ src/Brasa/RecursoHumanoBundle/Controller/PagoBancoController.php
@@ -317,12 +317,12 @@ class PagoBancoController extends Controller
         $arConfiguracionGeneral = new \Brasa\GeneralBundle\Entity\GenConfiguracion();
         $arConfiguracionGeneral = $em->getRepository('BrasaGeneralBundle:GenConfiguracion')->find(1);
         $strNombreArchivo = "pago" . date('YmdHis') . ".txt";
-        //$strArchivo = $arConfiguracionGeneral->getRutaTemporal() . $strNombreArchivo;                                    
-        $strArchivo = "c:/xampp/" . $strNombreArchivo;                                    
+        $strArchivo = $arConfiguracionGeneral->getRutaTemporal() . $strNombreArchivo;                                    
+        //$strArchivo = "c:/xampp/" . $strNombreArchivo;                                    
         $ar = fopen($strArchivo,"a") or die("Problemas en la creacion del archivo plano");
         // Encabezado
         $strNitEmpresa = $this->RellenarNr($arConfiguracionGeneral->getNitEmpresa(),"0",10);
-        $strNombreEmpresa = $this->RellenarNr(utf8_decode($arConfiguracionGeneral->getNombreEmpresa()), 0, 16);
+        $strNombreEmpresa = $this->RellenarNr(utf8_decode(substr($arConfiguracionGeneral->getNombreEmpresa(), 0, 16)), 0, 16);
         $strTipoPagoSecuencia = "225PAGO NOMI ";
         $strSecuencia = $arPagoBanco->getSecuencia();
         $strFechaCreacion = $arPagoBanco->getFechaTrasmision()->format('ymd');                                                                                            
@@ -331,25 +331,25 @@ class PagoBancoController extends Controller
         $strValorTotal = $this->RellenarNr(round($arPagoBanco->getVrTotalPago()), "0", 24);
         //Fin encabezado
         //(1) Tipo de registro, (10) Nit empresa, (225PAGO NOMI) Tipo pago secuencia secuencia, (yymmdd) fecha creacion, (yymmdd) fecha aplicacion, (), ()
-        fputs($ar, "1" . $strNitEmpresa . $strNombreEmpresa . $strTipoPagoSecuencia . $strFechaCreacion . $strSecuencia . $strFechaAplicacion . $strNumeroRegistros . $strValorTotal . $arPagoBanco->getCuentaRel()->getCuenta() . $arPagoBanco->getCuentaRel()->getTipo());
+        fputs($ar, "1" . $strNitEmpresa . $strNombreEmpresa . $strTipoPagoSecuencia . $strFechaCreacion . $strSecuencia . $strFechaAplicacion . $strNumeroRegistros . $strValorTotal . $arPagoBanco->getCuentaRel()->getCuenta() . $arPagoBanco->getCuentaRel()->getTipo() . "\n");
         //Inicio cuerpo
         $arPagosBancoDetalle = new \Brasa\RecursoHumanoBundle\Entity\RhuPagoBancoDetalle();
         $arPagosBancoDetalle = $em->getRepository('BrasaRecursoHumanoBundle:RhuPagoBancoDetalle')->findBy(array ('codigoPagoBancoFk' => $arPagoBanco->getCodigoPagoBancoPk()));                        
         foreach ($arPagosBancoDetalle AS $arPagoBancoDetalle) {
             fputs($ar, "6"); //(1)Tipo registro            
             fputs($ar, $this->RellenarNr($arPagoBancoDetalle->getNumeroIdentificacion(), "0", 15)); //(15) Nit del beneficiario           
-            fputs($ar, $this->RellenarNr(utf8_decode($arPagoBancoDetalle->getNombreCorto()),0, 18));
+            fputs($ar, $this->RellenarNr(utf8_decode(substr($arPagoBancoDetalle->getNombreCorto(), 0, 18)),"0", 18));
             fputs($ar, "005600078");
             fputs($ar, $this->RellenarNr($arPagoBancoDetalle->getCuenta(), "0", 17));
             fputs($ar, "S37");
             $duoValorNetoPagar = round($arPagoBancoDetalle->getVrPago());
             fputs($ar, ($this->RellenarNr($duoValorNetoPagar, "0", 10)));
-            fputs($ar, " ");
+            fputs($ar, "                      ");
             fputs($ar, "\n");
         }
+        fclose($ar);
         $em->flush();
-        //Fin cuerpo
-        fclose($ar);                
+        //Fin cuerpo                        
         header('Content-Description: File Transfer');
         header('Content-Type: text/csv; charset=ISO-8859-15');
         header('Content-Disposition: attachment; filename='.basename($strArchivo));
