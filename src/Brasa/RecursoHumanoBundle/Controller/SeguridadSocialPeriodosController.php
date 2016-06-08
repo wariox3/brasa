2043da631f677cb0e1784d5b0e44487b78707e5f
coordsistemas <coordsistemas@pc03-PC.JGEFECTIVOS.LOCAL>
2016-06-08 17:42:16 -0500

    archivo plano seguridad social 8

--- src/Brasa/RecursoHumanoBundle/Controller/SeguridadSocialPeriodosController.php
+++ src/Brasa/RecursoHumanoBundle/Controller/SeguridadSocialPeriodosController.php
@@ -176,8 +176,8 @@ class SeguridadSocialPeriodosController extends Controller
                 $codigoPeriodoDetalle = $request->request->get('OpGenerarArchivo');
                 $arConfiguracion = new \Brasa\GeneralBundle\Entity\GenConfiguracion();
                 $arConfiguracion = $em->getRepository('BrasaGeneralBundle:GenConfiguracion')->find(1);
-                //$strRutaArchivo = $arConfiguracion->getRutaTemporal();
-                $strRutaArchivo = "c:/xampp/";
+                $strRutaArchivo = $arConfiguracion->getRutaTemporal();
+                //$strRutaArchivo = "c:/xampp/";
                 $strNombreArchivo = "pila" . date('YmdHis') . ".txt";
                 $ar = fopen($strRutaArchivo . $strNombreArchivo, "a") or
                     die("Problemas en la creacion del archivo plano");
