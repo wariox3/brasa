932c1f1ac52cf5eed966d234e1ac539b25737e70
desarrollo <desarrollo@desarrollo-H81M-H>
2016-08-04 10:42:10 -0500

    Interface ofimatica

--- src/Brasa/TurnoBundle/Controller/Proceso/GenerarPedidoController.php
+++ src/Brasa/TurnoBundle/Controller/Proceso/GenerarPedidoController.php
@@ -258,7 +258,7 @@ class GenerarPedidoController extends Controller
             } 
             
             if ($form->get('BtnExcel')->isClicked()) {
-                $this->filtrar($form);
+                //$this->filtrar($form);
                 $this->lista($form);
                 $this->generarExcel();
             }
@@ -302,5 +302,79 @@ class GenerarPedidoController extends Controller
             ->getForm();
         return $form;
     }        
+    
+    private function generarExcel() {
+        $objFunciones = new \Brasa\GeneralBundle\MisClases\Funciones();
+        ob_clean();
+        $em = $this->getDoctrine()->getManager();        
+        $objPHPExcel = new \PHPExcel();
+        // Set document properties
+        $objPHPExcel->getProperties()->setCreator("EMPRESA")
+            ->setLastModifiedBy("EMPRESA")
+            ->setTitle("Office 2007 XLSX Test Document")
+            ->setSubject("Office 2007 XLSX Test Document")
+            ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+            ->setKeywords("office 2007 openxml php")
+            ->setCategory("Test result file");
+        $objPHPExcel->getDefaultStyle()->getFont()->setName('Arial')->setSize(9); 
+        $objPHPExcel->getActiveSheet()->getStyle('1')->getFont()->setBold(true);
+        for($col = 'A'; $col !== 'K'; $col++) {
+            $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);                           
+        }     
+        for($col = 'D'; $col !== 'K'; $col++) {
+            $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
+            $objPHPExcel->getActiveSheet()->getStyle($col)->getNumberFormat()->setFormatCode('#,##0');
+        }        
+               
+        $objPHPExcel->setActiveSheetIndex(0)                    
+                    ->setCellValue('A1', 'CÓDIG0')
+                    ->setCellValue('B1', 'NIT')
+                    ->setCellValue('C1', 'CLIENTE')
+                    ->setCellValue('D1', 'SECTOR') 
+                    ->setCellValue('E1', 'AUT')
+                    ->setCellValue('F1', 'CER')
+                    ->setCellValue('G1', 'HORAS')
+                    ->setCellValue('H1', 'H.DIURNAS')
+                    ->setCellValue('I1', 'H.NOCTURNAS')
+                    ->setCellValue('J1', 'VALOR');
+
+        $i = 2;
+        $query = $em->createQuery($this->strListaDql);
+        $arServicios = new \Brasa\TurnoBundle\Entity\TurServicio();
+        $arServicios = $query->getResult();
+
+        foreach ($arServicios as $arServicio) {            
+            $objPHPExcel->setActiveSheetIndex(0)
+                    ->setCellValue('A' . $i, $arServicio->getCodigoServicioPk())  
+                    ->setCellValue('B' . $i, $arServicio->getClienteRel()->getNit())
+                    ->setCellValue('C' . $i, $arServicio->getClienteRel()->getNombreCorto())
+                    ->setCellValue('D' . $i, $arServicio->getSectorRel()->getNombre())  
+                    ->setCellValue('E' . $i, $objFunciones->devuelveBoolean($arServicio->getEstadoAutorizado()))
+                    ->setCellValue('F' . $i, $objFunciones->devuelveBoolean($arServicio->getEstadoCerrado()))
+                    ->setCellValue('G' . $i, $arServicio->getHoras())
+                    ->setCellValue('H' . $i, $arServicio->getHorasDiurnas())
+                    ->setCellValue('I' . $i, $arServicio->getHorasNocturnas())
+                    ->setCellValue('J' . $i, $arServicio->getVrTotal());
+
+            $i++;
+        }
+
+        $objPHPExcel->getActiveSheet()->setTitle('Servicios');
+        $objPHPExcel->setActiveSheetIndex(0);
+        // Redirect output to a client’s web browser (Excel2007)
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="Servicios.xlsx"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+        // If you're serving to IE over SSL, then the following may be needed
+        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header ('Pragma: public'); // HTTP/1.0
+        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
+        $objWriter->save('php://output');
+        exit;
+    }    
     
 }
