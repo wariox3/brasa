b1a2f2e9a27c4b52d952f6aab12d1805863e1cad
pc <pc@pc>
2016-10-07 07:42:34 -0500

    tipo adicional requerido 2

--- src/Brasa/RecursoHumanoBundle/Entity/RhuPagoAdicional.php
+++ src/Brasa/RecursoHumanoBundle/Entity/RhuPagoAdicional.php
@@ -123,4 +123,494 @@ class RhuPagoAdicional
     
 
 
+
+    /**
+     * Get codigoPagoAdicionalPk
+     *
+     * @return integer
+     */
+    public function getCodigoPagoAdicionalPk()
+    {
+        return $this->codigoPagoAdicionalPk;
+    }
+
+    /**
+     * Set codigoPagoConceptoFk
+     *
+     * @param integer $codigoPagoConceptoFk
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setCodigoPagoConceptoFk($codigoPagoConceptoFk)
+    {
+        $this->codigoPagoConceptoFk = $codigoPagoConceptoFk;
+
+        return $this;
+    }
+
+    /**
+     * Get codigoPagoConceptoFk
+     *
+     * @return integer
+     */
+    public function getCodigoPagoConceptoFk()
+    {
+        return $this->codigoPagoConceptoFk;
+    }
+
+    /**
+     * Set codigoEmpleadoFk
+     *
+     * @param integer $codigoEmpleadoFk
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setCodigoEmpleadoFk($codigoEmpleadoFk)
+    {
+        $this->codigoEmpleadoFk = $codigoEmpleadoFk;
+
+        return $this;
+    }
+
+    /**
+     * Get codigoEmpleadoFk
+     *
+     * @return integer
+     */
+    public function getCodigoEmpleadoFk()
+    {
+        return $this->codigoEmpleadoFk;
+    }
+
+    /**
+     * Set tipoAdicional
+     *
+     * @param integer $tipoAdicional
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setTipoAdicional($tipoAdicional)
+    {
+        $this->tipoAdicional = $tipoAdicional;
+
+        return $this;
+    }
+
+    /**
+     * Get tipoAdicional
+     *
+     * @return integer
+     */
+    public function getTipoAdicional()
+    {
+        return $this->tipoAdicional;
+    }
+
+    /**
+     * Set modalidad
+     *
+     * @param integer $modalidad
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setModalidad($modalidad)
+    {
+        $this->modalidad = $modalidad;
+
+        return $this;
+    }
+
+    /**
+     * Get modalidad
+     *
+     * @return integer
+     */
+    public function getModalidad()
+    {
+        return $this->modalidad;
+    }
+
+    /**
+     * Set codigoPeriodoFk
+     *
+     * @param integer $codigoPeriodoFk
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setCodigoPeriodoFk($codigoPeriodoFk)
+    {
+        $this->codigoPeriodoFk = $codigoPeriodoFk;
+
+        return $this;
+    }
+
+    /**
+     * Get codigoPeriodoFk
+     *
+     * @return integer
+     */
+    public function getCodigoPeriodoFk()
+    {
+        return $this->codigoPeriodoFk;
+    }
+
+    /**
+     * Set fecha
+     *
+     * @param \DateTime $fecha
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setFecha($fecha)
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    /**
+     * Get fecha
+     *
+     * @return \DateTime
+     */
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    /**
+     * Set cantidad
+     *
+     * @param float $cantidad
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setCantidad($cantidad)
+    {
+        $this->cantidad = $cantidad;
+
+        return $this;
+    }
+
+    /**
+     * Get cantidad
+     *
+     * @return float
+     */
+    public function getCantidad()
+    {
+        return $this->cantidad;
+    }
+
+    /**
+     * Set valor
+     *
+     * @param float $valor
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setValor($valor)
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+
+    /**
+     * Get valor
+     *
+     * @return float
+     */
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    /**
+     * Set codigoProgramacionPagoFk
+     *
+     * @param integer $codigoProgramacionPagoFk
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setCodigoProgramacionPagoFk($codigoProgramacionPagoFk)
+    {
+        $this->codigoProgramacionPagoFk = $codigoProgramacionPagoFk;
+
+        return $this;
+    }
+
+    /**
+     * Get codigoProgramacionPagoFk
+     *
+     * @return integer
+     */
+    public function getCodigoProgramacionPagoFk()
+    {
+        return $this->codigoProgramacionPagoFk;
+    }
+
+    /**
+     * Set permanente
+     *
+     * @param boolean $permanente
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setPermanente($permanente)
+    {
+        $this->permanente = $permanente;
+
+        return $this;
+    }
+
+    /**
+     * Get permanente
+     *
+     * @return boolean
+     */
+    public function getPermanente()
+    {
+        return $this->permanente;
+    }
+
+    /**
+     * Set aplicaDiaLaborado
+     *
+     * @param boolean $aplicaDiaLaborado
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setAplicaDiaLaborado($aplicaDiaLaborado)
+    {
+        $this->aplicaDiaLaborado = $aplicaDiaLaborado;
+
+        return $this;
+    }
+
+    /**
+     * Get aplicaDiaLaborado
+     *
+     * @return boolean
+     */
+    public function getAplicaDiaLaborado()
+    {
+        return $this->aplicaDiaLaborado;
+    }
+
+    /**
+     * Set detalle
+     *
+     * @param string $detalle
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setDetalle($detalle)
+    {
+        $this->detalle = $detalle;
+
+        return $this;
+    }
+
+    /**
+     * Get detalle
+     *
+     * @return string
+     */
+    public function getDetalle()
+    {
+        return $this->detalle;
+    }
+
+    /**
+     * Set prestacional
+     *
+     * @param boolean $prestacional
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setPrestacional($prestacional)
+    {
+        $this->prestacional = $prestacional;
+
+        return $this;
+    }
+
+    /**
+     * Get prestacional
+     *
+     * @return boolean
+     */
+    public function getPrestacional()
+    {
+        return $this->prestacional;
+    }
+
+    /**
+     * Set estadoInactivo
+     *
+     * @param boolean $estadoInactivo
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setEstadoInactivo($estadoInactivo)
+    {
+        $this->estadoInactivo = $estadoInactivo;
+
+        return $this;
+    }
+
+    /**
+     * Get estadoInactivo
+     *
+     * @return boolean
+     */
+    public function getEstadoInactivo()
+    {
+        return $this->estadoInactivo;
+    }
+
+    /**
+     * Set codigoUsuario
+     *
+     * @param string $codigoUsuario
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setCodigoUsuario($codigoUsuario)
+    {
+        $this->codigoUsuario = $codigoUsuario;
+
+        return $this;
+    }
+
+    /**
+     * Get codigoUsuario
+     *
+     * @return string
+     */
+    public function getCodigoUsuario()
+    {
+        return $this->codigoUsuario;
+    }
+
+    /**
+     * Set fechaCreacion
+     *
+     * @param \DateTime $fechaCreacion
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setFechaCreacion($fechaCreacion)
+    {
+        $this->fechaCreacion = $fechaCreacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaCreacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaCreacion()
+    {
+        return $this->fechaCreacion;
+    }
+
+    /**
+     * Set fechaUltimaEdicion
+     *
+     * @param \DateTime $fechaUltimaEdicion
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setFechaUltimaEdicion($fechaUltimaEdicion)
+    {
+        $this->fechaUltimaEdicion = $fechaUltimaEdicion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaUltimaEdicion
+     *
+     * @return \DateTime
+     */
+    public function getFechaUltimaEdicion()
+    {
+        return $this->fechaUltimaEdicion;
+    }
+
+    /**
+     * Set pagoConceptoRel
+     *
+     * @param \Brasa\RecursoHumanoBundle\Entity\RhuPagoConcepto $pagoConceptoRel
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setPagoConceptoRel(\Brasa\RecursoHumanoBundle\Entity\RhuPagoConcepto $pagoConceptoRel = null)
+    {
+        $this->pagoConceptoRel = $pagoConceptoRel;
+
+        return $this;
+    }
+
+    /**
+     * Get pagoConceptoRel
+     *
+     * @return \Brasa\RecursoHumanoBundle\Entity\RhuPagoConcepto
+     */
+    public function getPagoConceptoRel()
+    {
+        return $this->pagoConceptoRel;
+    }
+
+    /**
+     * Set empleadoRel
+     *
+     * @param \Brasa\RecursoHumanoBundle\Entity\RhuEmpleado $empleadoRel
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setEmpleadoRel(\Brasa\RecursoHumanoBundle\Entity\RhuEmpleado $empleadoRel = null)
+    {
+        $this->empleadoRel = $empleadoRel;
+
+        return $this;
+    }
+
+    /**
+     * Get empleadoRel
+     *
+     * @return \Brasa\RecursoHumanoBundle\Entity\RhuEmpleado
+     */
+    public function getEmpleadoRel()
+    {
+        return $this->empleadoRel;
+    }
+
+    /**
+     * Set programacionPagoRel
+     *
+     * @param \Brasa\RecursoHumanoBundle\Entity\RhuProgramacionPago $programacionPagoRel
+     *
+     * @return RhuPagoAdicional
+     */
+    public function setProgramacionPagoRel(\Brasa\RecursoHumanoBundle\Entity\RhuProgramacionPago $programacionPagoRel = null)
+    {
+        $this->programacionPagoRel = $programacionPagoRel;
+
+        return $this;
+    }
+
+    /**
+     * Get programacionPagoRel
+     *
+     * @return \Brasa\RecursoHumanoBundle\Entity\RhuProgramacionPago
+     */
+    public function getProgramacionPagoRel()
+    {
+        return $this->programacionPagoRel;
+    }
 }
