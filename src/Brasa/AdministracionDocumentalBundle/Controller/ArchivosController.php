c18584d006833114cf0c814e7150737b42cb3d65
pc <pc@pc>
2016-10-19 09:07:02 -0500

    email 5

--- src/Brasa/AdministracionDocumentalBundle/Controller/ArchivosController.php
+++ src/Brasa/AdministracionDocumentalBundle/Controller/ArchivosController.php
@@ -144,7 +144,7 @@ class ArchivosController extends Controller
                         ->setBody($strMensaje,'text/html')                            
                         ->attach(\Swift_Attachment::fromPath($strRuta));                
                     $this->get('mailer')->send($message); 
-                    echo "Mensaje enviado con exito".$strMail;
+                    echo "Mensaje enviado con exito al correo ".$strMail;
                     //echo "<script languaje='javascript' type='text/javascript'>window.close();window.opener.location.reload();</script>";
                                                                                                 
             }
