4bbd3f306d3de283f52f7569956e5ac6d7f6c6fb
desarrollo <desarrollo@desarrollo-MS-7817>
2016-10-20 07:59:41 -0500

    Error vacaciones

--- src/Brasa/RecursoHumanoBundle/Controller/Consulta/EmpleadoCentroCostoMesController.php
+++ src/Brasa/RecursoHumanoBundle/Controller/Consulta/EmpleadoCentroCostoMesController.php
@@ -14,14 +14,14 @@ class EmpleadoCentroCostoMesController extends Controller
     var $intNumero = 0;
     /**
      * @Route("/rhu/consulta/empleado/centro/costo/mes", name="brs_rhu_consulta_empleado_centro_costo_mes")
-     */    
+     */
     public function listaAction() {
         $em = $this->getDoctrine()->getManager();
-        $request = $this->getRequest();       
+        $request = $this->getRequest();
         $paginator  = $this->get('knp_paginator');
         //if(!$em->getRepository('BrasaSeguridadBundle:SegUsuarioPermisoEspecial')->permisoEspecial($this->getUser(), 94)) {
-        //    return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));            
-        //}        
+        //    return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));
+        //}
         $form = $this->formularioLista();
         $form->handleRequest($request);
         $this->listar();
@@ -37,151 +37,41 @@ class EmpleadoCentroCostoMesController extends Controller
                 $this->listar();
             }
         }
-        $arConsultaPagoConcepto = $paginator->paginate($em->createQuery($this->strDqlLista), $request->query->get('page', 1), 50);
+        $arEmpleadoCentroCosto = $paginator->paginate($em->createQuery($this->strDqlLista), $request->query->get('page', 1), 50);
         return $this->render('BrasaRecursoHumanoBundle:Consultas/Empleados:centroCostoMes.html.twig', array(
-            'arConsultaPagoConcepto' => $arConsultaPagoConcepto,
+            'arEmpleadoCentroCosto' => $arEmpleadoCentroCosto,
             'form' => $form->createView()
             ));
-    }     
-    
+    }
+
     private function listar() {
         $session = $this->getRequest()->getSession();
         $em = $this->getDoctrine()->getManager();
-        $this->strDqlLista = $em->getRepository('BrasaRecursoHumanoBundle:RhuConsultaPagoConcepto')->listaDql();
-    }  
+        $this->strDqlLista = $em->getRepository('BrasaRecursoHumanoBundle:RhuEmpleadoCentroCosto')->listaDql();
+    }
 
     private function formularioLista() {
-        $em = $this->getDoctrine()->getManager();        
+        $em = $this->getDoctrine()->getManager();
         $session = $this->get('session');
-        $arrayPropiedadesPagoConcepto = array(
-                'class' => 'BrasaRecursoHumanoBundle:RhuPagoConcepto',
-                'query_builder' => function (EntityRepository $er) {
-                    return $er->createQueryBuilder('pc')
-                    ->orderBy('pc.nombre', 'ASC');},
-                'property' => 'nombre',
-                'required' => false,
-                'empty_data' => "",
-                'empty_value' => "TODOS",
-                'data' => ""
-            );
-        if($session->get('filtroCodigoPagoConcepto')) {
-            $arrayPropiedadesPagoConcepto['data'] = $em->getReference("BrasaRecursoHumanoBundle:RhuPagoConcepto", $session->get('filtroCodigoPagoConcepto'));
-        }
-        $dateFecha = new \DateTime('now');
-        $strFechaDesde = $dateFecha->format('Y-m-')."01";
-        $intUltimoDia = $strUltimoDiaMes = date("d",(mktime(0,0,0,$dateFecha->format('m')+1,1,$dateFecha->format('Y'))-1));
-        $strFechaHasta = $dateFecha->format('Y-m-').$intUltimoDia;
-        if($session->get('filtroDesde') != "") {
-            $strFechaDesde = $session->get('filtroDesde');
-        }
-        if($session->get('filtroHasta') != "") {
-            $strFechaHasta = $session->get('filtroHasta');
-        }    
-        $dateFechaDesde = date_create($strFechaDesde);
-        $dateFechaHasta = date_create($strFechaHasta);
-        $form = $this->createFormBuilder()                                    
-            ->add('pagoConceptoRel', 'entity', $arrayPropiedadesPagoConcepto)                        
-            ->add('fechaDesde', 'date', array('format' => 'yyyyMMdd', 'data' => $dateFechaDesde))                
-            ->add('fechaHasta', 'date', array('format' => 'yyyyMMdd', 'data' => $dateFechaHasta))                
-            ->add('BtnGenerar', 'submit', array('label'  => 'Filtrar'))                                        
-            ->add('TxtIdentificacion', 'text', array('label'  => 'Identificacion','data' => $session->get('filtroIdentificacion')))                                            
-            ->add('BtnExcel', 'submit', array('label'  => 'Excel',))            
-            ->getForm();        
+        $form = $this->createFormBuilder()
+            ->add('BtnGenerar', 'submit', array('label'  => 'Filtrar'))
+            ->add('TxtAnio', 'text')
+            ->add('TxtMes', 'text')
+            ->add('BtnExcel', 'submit', array('label'  => 'Excel',))
+            ->getForm();
         return $form;
     }
 
     private function filtrarLista($form, Request $request) {
-        $em = $this->getDoctrine()->getManager(); 
-        $session = $this->get('session');        
-        $strSql = "DELETE FROM rhu_consulta_pago_concepto WHERE 1";
-        $em->getConnection()->executeQuery($strSql);        
-        
-        $controles = $request->request->get('form');                
-        $session->set('filtroIdentificacion', $form->get('TxtIdentificacion')->getData());        
-        $dateFechaDesde = $form->get('fechaDesde')->getData();
-        $dateFechaHasta = $form->get('fechaHasta')->getData();
-        $session->set('filtroDesde', $dateFechaDesde->format('Y-m-d'));
-        $session->set('filtroHasta', $dateFechaHasta->format('Y-m-d'));
-        $session->set('filtroCodigoPagoConcepto', $controles['pagoConceptoRel']);
-        $dql = $em->getRepository('BrasaRecursoHumanoBundle:RhuPagoDetalle')->listaDetalleDql(
-                    "",
-                    "",
-                    $session->get('filtroIdentificacion'),
-                    "",
-                    $strFechaDesde = $session->get('filtroDesde'),
-                    $strFechaHasta = $session->get('filtroHasta'),
-                    $session->get('filtroCodigoPagoConcepto')
-                    );               
-        $query = $em->createQuery($dql);        
-        $arPagoDetalles = $query->getResult();   
-        foreach ($arPagoDetalles as $arPagoDetalle) {
-            $arConsultaPagoConcepto = new \Brasa\RecursoHumanoBundle\Entity\RhuConsultaPagoConcepto();
-            $arConsultaPagoConcepto->setOrigen('NOMINA');
-            $arConsultaPagoConcepto->setNumero($arPagoDetalle->getPagoRel()->getNumero());
-            $arConsultaPagoConcepto->setNumeroIdentificacion($arPagoDetalle->getPagoRel()->getEmpleadoRel()->getNumeroIdentificacion());
-            $arConsultaPagoConcepto->setNombreCorto($arPagoDetalle->getPagoRel()->getEmpleadoRel()->getNombreCorto());
-            $arConsultaPagoConcepto->setCodigoPagoConceptoFk($arPagoDetalle->getCodigoPagoConceptoFk());
-            $arConsultaPagoConcepto->setNombreConcepto($arPagoDetalle->getPagoConceptoRel()->getNombre());
-            $arConsultaPagoConcepto->setFechaDesde($arPagoDetalle->getPagoRel()->getFechaDesde());
-            $arConsultaPagoConcepto->setFechaHasta($arPagoDetalle->getPagoRel()->getFechaHasta());
-            if($arPagoDetalle->getOperacion() == -1) {
-                $arConsultaPagoConcepto->setVrDeduccion($arPagoDetalle->getVrPago());  
-            }
-            if($arPagoDetalle->getOperacion() == 1) {
-                $arConsultaPagoConcepto->setVrBonificacion($arPagoDetalle->getVrPago());  
-            }            
-            $em->persist($arConsultaPagoConcepto);            
-        }
-        $codigoEmpleado = "";
-        if($session->get('filtroIdentificacion')) {
-            $arEmpleado = $em->getRepository('BrasaRecursoHumanoBundle:RhuEmpleado')->findOneBy(array('numeroIdentificacion' => $session->get('filtroIdentificacion')));
-            if($arEmpleado) {
-               $codigoEmpleado =  $arEmpleado->getCodigoEmpleadoPk();
-            }
-        }
-        $dql = $em->getRepository('BrasaRecursoHumanoBundle:RhuVacacionAdicional')->listaConsultaDql($session->get('filtroDesde'), $session->get('filtroHasta'), $codigoEmpleado, $session->get('filtroCodigoPagoConcepto'));
-        $query = $em->createQuery($dql);        
-        $arVacacionesAdicionales = $query->getResult();   
-        foreach ($arVacacionesAdicionales as $arVacacionAdicional) {
-            $arConsultaPagoConcepto = new \Brasa\RecursoHumanoBundle\Entity\RhuConsultaPagoConcepto();
-            $arConsultaPagoConcepto->setOrigen('VACACION');
-            $arConsultaPagoConcepto->setNumero($arVacacionAdicional->getCodigoVacacionFk());            
-            $arConsultaPagoConcepto->setNumeroIdentificacion($arVacacionAdicional->getVacacionRel()->getEmpleadoRel()->getNumeroIdentificacion());
-            $arConsultaPagoConcepto->setNombreCorto($arVacacionAdicional->getVacacionRel()->getEmpleadoRel()->getNombreCorto());
-            $arConsultaPagoConcepto->setCodigoPagoConceptoFk($arVacacionAdicional->getCodigoPagoConceptoFk());
-            $arConsultaPagoConcepto->setNombreConcepto($arVacacionAdicional->getPagoConceptoRel()->getNombre());            
-            $arConsultaPagoConcepto->setVrBonificacion($arVacacionAdicional->getVrBonificacion());
-            $arConsultaPagoConcepto->setVrDeduccion($arVacacionAdicional->getVrDeduccion());
-            $arConsultaPagoConcepto->setFechaDesde($arVacacionAdicional->getVacacionRel()->getFecha());
-            $arConsultaPagoConcepto->setFechaHasta($arVacacionAdicional->getVacacionRel()->getFecha());            
-            $em->persist($arConsultaPagoConcepto);            
-        }        
-        $dql = $em->getRepository('BrasaRecursoHumanoBundle:RhuLiquidacionAdicionales')->listaConsultaDql($session->get('filtroDesde'), $session->get('filtroHasta'), $codigoEmpleado, $session->get('filtroCodigoPagoConcepto'));
-        $query = $em->createQuery($dql);        
-        $arLiquidacionAdicionales = $query->getResult();   
-        foreach ($arLiquidacionAdicionales as $arLiquidacionAdicional) {
-            $arConsultaPagoConcepto = new \Brasa\RecursoHumanoBundle\Entity\RhuConsultaPagoConcepto();
-            $arConsultaPagoConcepto->setOrigen('LIQUIDACION');
-            $arConsultaPagoConcepto->setNumero($arLiquidacionAdicional->getCodigoLiquidacionFk());            
-            $arConsultaPagoConcepto->setNumeroIdentificacion($arLiquidacionAdicional->getLiquidacionRel()->getEmpleadoRel()->getNumeroIdentificacion());
-            $arConsultaPagoConcepto->setNombreCorto($arLiquidacionAdicional->getLiquidacionRel()->getEmpleadoRel()->getNombreCorto());
-            $arConsultaPagoConcepto->setCodigoPagoConceptoFk($arLiquidacionAdicional->getCodigoPagoConceptoFk());
-            $arConsultaPagoConcepto->setNombreConcepto($arLiquidacionAdicional->getPagoConceptoRel()->getNombre());            
-            $arConsultaPagoConcepto->setVrBonificacion($arLiquidacionAdicional->getVrBonificacion());
-            $arConsultaPagoConcepto->setVrDeduccion($arLiquidacionAdicional->getVrDeduccion());
-            $arConsultaPagoConcepto->setFechaDesde($arLiquidacionAdicional->getLiquidacionRel()->getFecha());
-            $arConsultaPagoConcepto->setFechaHasta($arLiquidacionAdicional->getLiquidacionRel()->getFecha());            
-            $em->persist($arConsultaPagoConcepto);            
-        }        
-        $em->flush();
-        
+        $em = $this->getDoctrine()->getManager();
+        $session = $this->get('session');
     }
 
     private function generarExcel() {
         ob_clean();
         set_time_limit(0);
         ini_set("memory_limit", -1);
-        $em = $this->getDoctrine()->getManager(); 
+        $em = $this->getDoctrine()->getManager();
         $session = $this->get('session');
         $objPHPExcel = new \PHPExcel();
         // Set document properties
@@ -193,51 +83,43 @@ class EmpleadoCentroCostoMesController extends Controller
             ->setKeywords("office 2007 openxml php")
             ->setCategory("Test result file");
         for($col = 'A'; $col !== 'K'; $col++) {
-                    $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);                           
-        } 
-        for($col = 'I'; $col !== 'K'; $col++) {            
-            $objPHPExcel->getActiveSheet()->getStyle($col)->getNumberFormat()->setFormatCode('#,##0');
-        }          
+                    $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
+        }
+
         $objPHPExcel->getActiveSheet()->getStyle('1')->getFont()->setBold(true);
         $objPHPExcel->setActiveSheetIndex(0)
-                    ->setCellValue('A1', 'ORIGEN')
-                    ->setCellValue('B1', 'NUMERO')
-                    ->setCellValue('C1', 'IDENTIFICACIÓN')
-                    ->setCellValue('D1', 'EMPLEADO')
-                    ->setCellValue('E1', 'CODIGO')
-                    ->setCellValue('F1', 'CONCEPTO')
-                    ->setCellValue('G1', 'DESDE')
-                    ->setCellValue('H1', 'HASTA')
-                    ->setCellValue('I1', 'BONIFICACION')
-                    ->setCellValue('J1', 'DEDUCCION');
+                    ->setCellValue('A1', 'NIT')
+                    ->setCellValue('B1', 'ANIO')
+                    ->setCellValue('C1', 'MES')
+                    ->setCellValue('D1', 'PUESTO')
+                    ->setCellValue('E1', 'C_COSTO');
 
         $i = 2;
-        $dql   = "SELECT cpc FROM BrasaRecursoHumanoBundle:RhuConsultaPagoConcepto cpc";        
+        $dql   = $this->strDqlLista;
         $query = $em->createQuery($dql);
-        $arConsultaPagoConceptos = new \Brasa\RecursoHumanoBundle\Entity\RhuConsultaPagoConcepto();
-        $arConsultaPagoConceptos = $query->getResult();
-        
-        foreach ($arConsultaPagoConceptos as $arConsultaPagoConcepto) {            
+        $arEmpleadosCentroCosto = new \Brasa\RecursoHumanoBundle\Entity\RhuEmpleadoCentroCosto();
+        $arEmpleadosCentroCosto = $query->getResult();
+        foreach ($arEmpleadosCentroCosto as $arEmpleadoCentroCosto) {
+            $nit = "";
+            if($arEmpleadoCentroCosto->getCodigoEmpleadoFk()) {
+                $arEmpleado = $em->getRepository('BrasaRecursoHumanoBundle:RhuEmpleado')->find($arEmpleadoCentroCosto->getCodigoEmpleadoFk());
+                $nit = $arEmpleado->getNumeroIdentificacion() . "-" . $arEmpleado->getDigitoVerificacion();
+            }
             $objPHPExcel->setActiveSheetIndex(0)
-                    ->setCellValue('A' . $i, $arConsultaPagoConcepto->getOrigen())
-                    ->setCellValue('B' . $i, $arConsultaPagoConcepto->getNumero())                    
-                    ->setCellValue('C' . $i, $arConsultaPagoConcepto->getNumeroIdentificacion())
-                    ->setCellValue('D' . $i, $arConsultaPagoConcepto->getNombreCorto())
-                    ->setCellValue('E' . $i, $arConsultaPagoConcepto->getCodigoPagoConceptoFk())
-                    ->setCellValue('F' . $i, $arConsultaPagoConcepto->getNombreConcepto())
-                    ->setCellValue('G' . $i, $arConsultaPagoConcepto->getFechaDesde()->format('Y-m-d'))
-                    ->setCellValue('H' . $i, $arConsultaPagoConcepto->getFechaHasta()->format('Y-m-d'))
-                    ->setCellValue('I' . $i, $arConsultaPagoConcepto->getVrBonificacion())
-                    ->setCellValue('J' . $i, $arConsultaPagoConcepto->getVrDeduccion());
+                    ->setCellValue('A' . $i, $nit)
+                    ->setCellValue('B' . $i, $arEmpleadoCentroCosto->getAnio())
+                    ->setCellValue('C' . $i, $arEmpleadoCentroCosto->getMes())
+                    ->setCellValue('D' . $i, $arEmpleadoCentroCosto->getCodigoPuestoFk())
+                    ->setCellValue('E' . $i, $arEmpleadoCentroCosto->getCodigoCentroCostoFk());
             $i++;
         }
 
-        $objPHPExcel->getActiveSheet()->setTitle('conceptoPagoConsolidado');
+        $objPHPExcel->getActiveSheet()->setTitle('empleadoCentroCosto');
         $objPHPExcel->setActiveSheetIndex(0);
 
         // Redirect output to a client’s web browser (Excel2007)
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        header('Content-Disposition: attachment;filename="conceptoPagoConsolidado.xlsx"');
+        header('Content-Disposition: attachment;filename="empleadoCentroCosto.xlsx"');
         header('Cache-Control: max-age=0');
         // If you're serving to IE 9, then the following may be needed
         header('Cache-Control: max-age=1');
@@ -249,6 +131,6 @@ class EmpleadoCentroCostoMesController extends Controller
         $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
         $objWriter->save('php://output');
         exit;
-    }    
-    
+    }
+
 }
--- src/Brasa/RecursoHumanoBundle/Repository/RhuEmpleadoCentroCostoRepository.php
+++ src/Brasa/RecursoHumanoBundle/Repository/RhuEmpleadoCentroCostoRepository.php
@@ -11,5 +11,11 @@ use Doctrine\ORM\EntityRepository;
  */
 class RhuEmpleadoCentroCostoRepository extends EntityRepository {
 
+    public function listaDql() {        
+        $em = $this->getEntityManager();
+        $dql   = "SELECT ecc FROM BrasaRecursoHumanoBundle:RhuEmpleadoCentroCosto ecc WHERE ecc.codigoEmpleadoCentroCostoPk <> 0";               
+        $dql .= " ORDER BY ecc.anio, ecc.mes DESC";
+        return $dql;
+    }
     
 }
