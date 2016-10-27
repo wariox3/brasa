c9cb11d235fc43ce8357984bbb335efd2e881b60
pc <pc@pc>
2016-10-27 17:03:51 -0500

    formato estelar

--- src/Brasa/RecursoHumanoBundle/Formatos/FormatoProcesoDisciplinarioEstelar.php
+++ src/Brasa/RecursoHumanoBundle/Formatos/FormatoProcesoDisciplinarioEstelar.php
@@ -40,21 +40,21 @@ class FormatoProcesoDisciplinarioEstelar extends \FPDF_FPDF {
             $this->Line(10, 50, 60, 50);
             $this->Cell(0, 0, $this->Image('imagenes/logos/logo.jpg' , 15 ,20, 40 , 20,'JPG'), 0, 0, 'C', 0); //cuadro para el logo
             $this->SetXY(60, 10);
-            $this->Cell(90, 10, utf8_decode("PROCESO GESTIÓN HUMANA"), 1, 0, 'C', 1); //cuadro mitad arriba
+            $this->Cell(110, 10, utf8_decode("PROCESO GESTIÓN HUMANA"), 1, 0, 'C', 1); //cuadro mitad arriba
             $this->SetXY(60, 20);
             $this->SetFillColor(236, 236, 236);
             $this->Cell(90, 20, utf8_decode($arContenidoFormatoA->getTitulo()), 1, 0, 'C', 1); //cuardo mitad medio
             $this->SetFillColor(272, 272, 272);
             $this->SetXY(60, 40);
             $this->Cell(90, 10, utf8_decode(""), 1, 0, 'C', 1); //cuardo mitad abajo
-            $this->SetXY(150, 10);
-            $this->Cell(50, 10, utf8_decode('Página ') . $this->PageNo() . ' de {nb}', 1, 0, 'C', 1); //cuadro derecho arriba
-            $this->SetXY(150, 20);
-            $this->Cell(50, 20, utf8_decode($arContenidoFormatoA->getCodigoFormatoIso()), 1, 0, 'C', 1); //cuadro derecho mitad 1
-            $this->SetXY(150, 40);
-            $this->Cell(50, 5, utf8_decode("Versión ". $arContenidoFormatoA->getVersion() .""), 1, 0, 'C', 1); //cuadro derecho abajo 1
-            $this->SetXY(150, 45);
-            $this->Cell(50, 5, $arContenidoFormatoA->getFechaVersion()->format('Y-m-d'), 1, 0, 'C', 1); //cuadro derecho abajo 2
+            //$this->SetXY(150, 10);
+            //$this->Cell(50, 10, utf8_decode('Página ') . $this->PageNo() . ' de {nb}', 1, 0, 'C', 1); //cuadro derecho arriba
+            //$this->SetXY(150, 20);
+            //$this->Cell(50, 20, utf8_decode($arContenidoFormatoA->getCodigoFormatoIso()), 1, 0, 'C', 1); //cuadro derecho mitad 1
+            //$this->SetXY(150, 40);
+            //$this->Cell(50, 5, utf8_decode("Versión ". $arContenidoFormatoA->getVersion() .""), 1, 0, 'C', 1); //cuadro derecho abajo 1
+            //$this->SetXY(150, 45);
+            //$this->Cell(50, 5, $arContenidoFormatoA->getFechaVersion()->format('Y-m-d'), 1, 0, 'C', 1); //cuadro derecho abajo 2
         } else {
             $this->Image('imagenes/logos/logo.jpg' , 10 ,5, 50 , 30,'JPG');
             //$this->Image('imagenes/logos/encabezado.jpg' , 115 ,5, 90 , 40,'JPG');
@@ -68,7 +68,7 @@ class FormatoProcesoDisciplinarioEstelar extends \FPDF_FPDF {
         $arConfiguracion = new \Brasa\GeneralBundle\Entity\GenConfiguracion();
         $arConfiguracion = self::$em->getRepository('BrasaGeneralBundle:GenConfiguracion')->find(1);        
         $this->SetFont('Arial','','9');
-        $this->Text(10, 55, utf8_decode($arConfiguracion->getCiudadRel()->getNombre()). " ". date('Y-m-d'));
+        //$this->Text(10, 55, utf8_decode($arConfiguracion->getCiudadRel()->getNombre()). " ". date('Y-m-d'));
         $this->Ln(20);
     }
 
