220bf77bdef5008eca051425d980cff9da4701b1
pc <pc@pc>
2016-10-27 17:32:20 -0500

    formato estelar 2

--- src/Brasa/RecursoHumanoBundle/Formatos/FormatoProcesoDisciplinarioEstelar.php
+++ src/Brasa/RecursoHumanoBundle/Formatos/FormatoProcesoDisciplinarioEstelar.php
@@ -38,7 +38,7 @@ class FormatoProcesoDisciplinarioEstelar extends \FPDF_FPDF {
             $this->Line(10, 10, 100, 10);
             $this->Line(10, 10, 10, 50);
             $this->Line(10, 50, 60, 50);
-            //$this->Line(10, 10, 60, 10);
+            //$this->Line(10, 10, 60, 10); 
             $this->Cell(0, 0, $this->Image('imagenes/logos/logo.jpg' , 15 ,20, 40 , 20,'JPG'), 0, 0, 'C', 0); //cuadro para el logo
             $this->SetXY(60, 10);
             $this->Cell(140, 7, utf8_decode("SEGURIDAD ESTELAR LTDA"), 0, 0, 'C', 1); //cuadro mitad arriba
