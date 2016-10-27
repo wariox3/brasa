8d3635647bac637e5dd1f594de497fd114f072f1
desarrollo <desarrollo@desarrollo-MS-7817>
2016-10-27 14:59:40 -0500

    Generar programacion

--- src/Brasa/TurnoBundle/Controller/Proceso/GenerarSoportePagoController.php
+++ src/Brasa/TurnoBundle/Controller/Proceso/GenerarSoportePagoController.php
@@ -26,7 +26,7 @@ class GenerarSoportePagoController extends Controller
         $form->handleRequest($request); 
         $this->listaPeriodo();
         if ($form->isValid()) {
-            if($request->request->get('OpGenerarProgramacion')) {
+            /*if($request->request->get('OpGenerarProgramacion')) {
                 set_time_limit(0);
                 ini_set("memory_limit", -1);
                 $codigoSoportePagoPeriodo = $request->request->get('OpGenerarProgramacion');
@@ -40,6 +40,8 @@ class GenerarSoportePagoController extends Controller
                 $em->flush();                 
                 return $this->redirect($this->generateUrl('brs_tur_proceso_generar_soporte_pago'));
             }
+             * 
+             */
             if($request->request->get('OpGenerar')) {  
                 set_time_limit(0);
                 ini_set("memory_limit", -1);
