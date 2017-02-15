236dd956c1bae58bd5c066c7f65c435325c2de27
pc <pc@pc>
2017-02-15 10:08:13 -0500

    novedad

--- src/Brasa/AfiliacionBundle/Controller/Movimiento/NovedadController.php
+++ src/Brasa/AfiliacionBundle/Controller/Movimiento/NovedadController.php
@@ -13,21 +13,21 @@ class NovedadController extends Controller
     var $strDqlLista = "";
     /**
      * @Route("/afi/movimiento/novedad", name="brs_afi_movimiento_novedad")
-     */    
+     */
     public function listaAction(Request $request) {
-        $em = $this->getDoctrine()->getManager();        
+        $em = $this->getDoctrine()->getManager();
         $paginator  = $this->get('knp_paginator');
         if(!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 129, 1)) {
-            return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));            
+            return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));
         }
         $form = $this->formularioFiltro();
         $form->handleRequest($request);
         $this->lista();
         if ($form->isValid()) {
-            $arrSeleccionados = $request->request->get('ChkSeleccionar');                                               
+            $arrSeleccionados = $request->request->get('ChkSeleccionar');
             if ($form->get('BtnEliminar')->isClicked()) {
                 if(!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 129, 4)) {
-                    return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));            
+                    return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));
                 }
                 $arrSeleccionados = $request->request->get('ChkSeleccionar');
                 $em->getRepository('BrasaAfiliacionBundle:AfiNovedad')->eliminar($arrSeleccionados);
@@ -41,33 +41,33 @@ class NovedadController extends Controller
                 $this->generarExcel();
             }
         }
-        
+
         $arNovedades = $paginator->paginate($em->createQuery($this->strDqlLista), $request->query->get('page', 1), 20);
         return $this->render('BrasaAfiliacionBundle:Movimiento/Novedad:lista.html.twig', array(
-            'arNovedades' => $arNovedades, 
+            'arNovedades' => $arNovedades,
             'form' => $form->createView()));
     }
 
     /**
      * @Route("/afi/movimiento/novedad/nuevo/{codigoNovedad}", name="brs_afi_movimiento_novedad_nuevo")
-     */    
+     */
     public function nuevoAction(Request $request, $codigoNovedad = '') {
         $em = $this->getDoctrine()->getManager();
         $objMensaje = new \Brasa\GeneralBundle\MisClases\Mensajes();
         $arNovedad = new \Brasa\AfiliacionBundle\Entity\AfiNovedad();
         if($codigoNovedad != '' && $codigoNovedad != '0') {
             if(!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 129, 3)) {
-               return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));            
+               return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));
             }
             $arNovedad = $em->getRepository('BrasaAfiliacionBundle:AfiNovedad')->find($codigoNovedad);
         } else {
             if(!$em->getRepository('BrasaSeguridadBundle:SegPermisoDocumento')->permiso($this->getUser(), 129, 2)) {
-                return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));            
+                return $this->redirect($this->generateUrl('brs_seg_error_permiso_especial'));
             }
             $fecha = new \DateTime('now');
             $arNovedad->setFechaDesde($fecha);
             $arNovedad->setFechaHasta($fecha);
-        }        
+        }
         $form = $this->createForm(new AfiNovedadType, $arNovedad);
         $form->handleRequest($request);
         if ($form->isValid()) {
@@ -75,58 +75,66 @@ class NovedadController extends Controller
             $arrControles = $request->request->All();
             if($arrControles['txtNumeroIdentificacion'] != '') {
                 $arEmpleado = new \Brasa\AfiliacionBundle\Entity\AfiEmpleado();
-                $arEmpleado = $em->getRepository('BrasaAfiliacionBundle:AfiEmpleado')->findOneBy(array('numeroIdentificacion' => $arrControles['txtNumeroIdentificacion']));                                        
+                $arEmpleado = $em->getRepository('BrasaAfiliacionBundle:AfiEmpleado')->findOneBy(array('numeroIdentificacion' => $arrControles['txtNumeroIdentificacion']));
                 if(count($arEmpleado) > 0) {
                     $arNovedad->setEmpleadoRel($arEmpleado);
-                    if($arEmpleado->getEstadoActivo() == 1) {
+                    if($arEmpleado->getCodigoEmpleadoPk()) {
                         $arContrato = new \Brasa\AfiliacionBundle\Entity\AfiContrato();
-                        $arContrato = $em->getRepository('BrasaAfiliacionBundle:AfiContrato')->find($arEmpleado->getCodigoContratoActivo());                                                                
-                        $arNovedad->setContratoRel($arContrato);
-                        $em->persist($arNovedad);
-                        $em->flush();
+                        $arContrato = $em->getRepository('BrasaAfiliacionBundle:AfiContrato')->findOneBy(array('codigoEmpleadoFk' => $arEmpleado->getCodigoEmpleadoPk(), 'estadoActivo' => 1));
+                        if ($arContrato){
+                            $arNovedad->setContratoRel($arContrato);
+                            $em->persist($arNovedad);
+                            $em->flush();
+                        } else {
+                            $objMensaje->Mensaje("error", "El empleado con numero identificacion " .$arrControles['txtNumeroIdentificacion'] ." no tiene contrato activo", $this);
+                            return $this->redirect($this->generateUrl('brs_afi_movimiento_novedad_nuevo', array('codigoNovedad' => 0 )));
+                        }                        
                     } else {
                         $objMensaje->Mensaje("error", "El empleado con numero identificacion " .$arrControles['txtNumeroIdentificacion'] ." no tiene contrato activo", $this);
                         return $this->redirect($this->generateUrl('brs_afi_movimiento_novedad_nuevo', array('codigoNovedad' => 0 )));
-                    }                    
-                }   
+                    }
+                } else {
+                    $objMensaje->Mensaje("error", "El numero identificacion " .$arrControles['txtNumeroIdentificacion'] ." no registra en el sistema", $this);
+                    return $this->redirect($this->generateUrl('brs_afi_movimiento_novedad_nuevo', array('codigoNovedad' => 0 )));
+                }
             }
-            
-                        
+
+
             if($form->get('guardarnuevo')->isClicked()) {
                 return $this->redirect($this->generateUrl('brs_afi_movimiento_novedad_nuevo', array('codigoNovedad' => 0 )));
             } else {
                 return $this->redirect($this->generateUrl('brs_afi_movimiento_novedad'));
-            }                                   
+            }
         }
         return $this->render('BrasaAfiliacionBundle:Movimiento/Novedad:nuevo.html.twig', array(
             'arNovedad' => $arNovedad,
             'form' => $form->createView()));
-    }        
-    
-    private function lista() {    
+    }
+
+    private function lista() {
         $session = new session;
         $em = $this->getDoctrine()->getManager();
         $this->strDqlLista = $em->getRepository('BrasaAfiliacionBundle:AfiNovedad')->listaDQL(
-                  
-                ); 
-    }   
-    
-    private function filtrar ($form) {        
-        $session = new session;    
+
+                );
+    }
+
+    private function filtrar ($form) {
+        $session = new session;
         $session->set('filtroNovedadNombre', $form->get('TxtNombre')->getData());
         $this->lista();
     }
-    
+
     private function formularioFiltro() {
         $session = new session;
-        $form = $this->createFormBuilder()            
+        $form = $this->createFormBuilder()
             ->add('TxtNombre', textType::class, array('label'  => 'Nombre','data' => $session->get('filtroNovedadNombre')))
-            ->add('BtnEliminar', SubmitType::class, array('label'  => 'Eliminar',))            
+            ->add('BtnEliminar', SubmitType::class, array('label'  => 'Eliminar',))
             ->add('BtnExcel', SubmitType::class, array('label'  => 'Excel',))
             ->add('BtnFiltrar', SubmitType::class, array('label'  => 'Filtrar'))
             ->getForm();
         return $form;
-    }        
+    }
 
     private function generarExcel() {
         ob_clean();
@@ -141,32 +149,32 @@ class NovedadController extends Controller
             ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
             ->setKeywords("office 2007 openxml php")
             ->setCategory("Test result file");
-        $objPHPExcel->getDefaultStyle()->getFont()->setName('Arial')->setSize(10); 
+        $objPHPExcel->getDefaultStyle()->getFont()->setName('Arial')->setSize(10);
         $objPHPExcel->getActiveSheet()->getStyle('1')->getFont()->setBold(true);
         for($col = 'A'; $col !== 'C'; $col++) {
-            $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);         
-        }      
-        /*for($col = 'AI'; $col !== 'AK'; $col++) {            
+            $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
+        }
+        /*for($col = 'AI'; $col !== 'AK'; $col++) {
             $objPHPExcel->getActiveSheet()->getStyle($col)->getNumberFormat()->setFormatCode('#,##0');
-        }*/         
-        
+        }*/
+
         $objPHPExcel->setActiveSheetIndex(0)
                     ->setCellValue('A1', 'CÓDIG0')
                     ->setCellValue('B1', 'TIPO');
 
         $i = 2;
-        
+
         $query = $em->createQuery($this->strDqlLista);
         $arNovedades = new \Brasa\AfiliacionBundle\Entity\AfiNovedad();
         $arNovedades = $query->getResult();
-                
-        foreach ($arNovedades as $arNovedad) {            
+
+        foreach ($arNovedades as $arNovedad) {
             $objPHPExcel->setActiveSheetIndex(0)
                     ->setCellValue('A' . $i, $arNovedad->getCodigoNovedadPk())
-                    ->setCellValue('B' . $i, $arNovedad->getNovedadTipoRel()->getNombre());                                    
+                    ->setCellValue('B' . $i, $arNovedad->getNovedadTipoRel()->getNombre());
             $i++;
         }
-        
+
         $objPHPExcel->getActiveSheet()->setTitle('Novedad');
         $objPHPExcel->setActiveSheetIndex(0);
         // Redirect output to a client’s web browser (Excel2007)
@@ -180,10 +188,10 @@ class NovedadController extends Controller
         header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
         header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
         header ('Pragma: public'); // HTTP/1.0
-        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);        
+        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
         $objWriter->save('php://output');
         exit;
     }
 
-    
+
 }
