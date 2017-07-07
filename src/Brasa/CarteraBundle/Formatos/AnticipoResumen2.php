94b4e542fa780b2c25fadde1c5bbfe693a99a30c
pc--25 <pc--25@pc25-H81M-H>
2017-07-07 08:38:17 -0500

    Impresion Anticipos - estado anulado y totales

--- src/Brasa/CarteraBundle/Formatos/AnticipoResumen2.php
+++ src/Brasa/CarteraBundle/Formatos/AnticipoResumen2.php
@@ -1,12 +1,14 @@
 <?php
+
 namespace Brasa\CarteraBundle\Formatos;
+
 class AnticipoResumen2 extends \FPDF_FPDF {
+
     public static $em;
-    
     public static $fechaDesde;
     public static $fechaHasta;
-    
-    public function Generar($miThis, $fechaDesde, $fechaHasta) {        
+
+    public function Generar($miThis, $fechaDesde, $fechaHasta) {
         ob_clean();
         $em = $miThis->getDoctrine()->getManager();
         self::$em = $em;
@@ -14,66 +16,64 @@ class AnticipoResumen2 extends \FPDF_FPDF {
         self::$fechaHasta = $fechaHasta;
         $pdf = new AnticipoResumen2();
         $pdf->AliasNbPages();
-        $pdf->AddPage('P','Letter');
+        $pdf->AddPage('P', 'Letter');
         $pdf->SetFont('Times', '', 12);
         $this->Body($pdf);
 
-        $pdf->Output("AnticipoResumen.pdf", 'D');        
-        
-    } 
-    
+        $pdf->Output("AnticipoResumen.pdf", 'D');
+    }
+
     public function Header() {
         $arConfiguracion = new \Brasa\GeneralBundle\Entity\GenConfiguracion();
         $arConfiguracion = self::$em->getRepository('BrasaGeneralBundle:GenConfiguracion')->find(1);
-        $this->SetFillColor(200, 200, 200);        
-        $this->SetFont('Arial','B',10);
+        $this->SetFillColor(200, 200, 200);
+        $this->SetFont('Arial', 'B', 10);
         //Logo
         $this->SetXY(53, 10);
         $this->Image('imagenes/logos/logo.jpg', 12, 7, 35, 17);
         //INFORMACIÓN EMPRESA
         $this->Cell(150, 7, utf8_decode("RESUMEN RECIBOS ANTICIPOS"), 0, 0, 'C', 1);
         $this->SetXY(53, 18);
-        $this->SetFont('Arial','B',9);
+        $this->SetFont('Arial', 'B', 9);
         $this->Cell(20, 4, "EMPRESA:", 0, 0, 'L', 1);
         $this->Cell(100, 4, $arConfiguracion->getNombreEmpresa(), 0, 0, 'L', 0);
         $this->SetXY(53, 22);
         $this->Cell(20, 4, "NIT:", 0, 0, 'L', 1);
-        $this->Cell(100, 4, $arConfiguracion->getNitEmpresa()." - ". $arConfiguracion->getDigitoVerificacionEmpresa(), 0, 0, 'L', 0);
+        $this->Cell(100, 4, $arConfiguracion->getNitEmpresa() . " - " . $arConfiguracion->getDigitoVerificacionEmpresa(), 0, 0, 'L', 0);
         $this->SetXY(53, 26);
         $this->Cell(20, 4, utf8_decode("DIRECCIÓN:"), 0, 0, 'L', 1);
         $this->Cell(100, 4, $arConfiguracion->getDireccionEmpresa(), 0, 0, 'L', 0);
         $this->SetXY(53, 30);
         $this->Cell(20, 4, utf8_decode("TELÉFONO:"), 0, 0, 'L', 1);
-        $this->Cell(100, 4, $arConfiguracion->getTelefonoEmpresa(), 0, 0, 'L', 0);        
-        
+        $this->Cell(100, 4, $arConfiguracion->getTelefonoEmpresa(), 0, 0, 'L', 0);
+
         //Fecha y hora de impresion
         $this->SetFillColor(272, 272, 272);
-        $this->SetFont('Arial','B',7);
+        $this->SetFont('Arial', 'B', 7);
         $this->SetXY(10, 35);
-        $this->Cell(20, 4, utf8_decode("Impresion:". date('Y-m-d H:i:s') .""), 0, 0, 'L', 1);
-        $this->SetFillColor(236, 236, 236);        
-        $this->SetFont('Arial','B',10);
+        $this->Cell(20, 4, utf8_decode("Impresion:" . date('Y-m-d H:i:s') . ""), 0, 0, 'L', 1);
+        $this->SetFillColor(236, 236, 236);
+        $this->SetFont('Arial', 'B', 10);
         //Fin fecha y hora impresion
-        
+
         $intY = 40;
         //linea 1
-        $this->SetFillColor(272, 272, 272); 
+        $this->SetFillColor(272, 272, 272);
         $this->SetXY(10, $intY);
-        $this->SetFont('Arial','B',8);
-        $this->Cell(26, 5, utf8_decode("FECHA DESDE:") , 1, 0, 'L', 1);
-        $this->SetFont('Arial',  '',8);
+        $this->SetFont('Arial', 'B', 8);
+        $this->Cell(26, 5, utf8_decode("FECHA DESDE:"), 1, 0, 'L', 1);
+        $this->SetFont('Arial', '', 8);
         $this->Cell(52, 5, self::$fechaDesde, 1, 0, 'L', 1);
-       
+
         //linea 2
-        $this->SetXY(10, $intY+5);
-        $this->SetFont('Arial','B',8);
-        $this->Cell(26, 5, utf8_decode("FECHA HASTA:") , 1, 0, 'L', 1);
-        $this->SetFont('Arial','',8);
+        $this->SetXY(10, $intY + 5);
+        $this->SetFont('Arial', 'B', 8);
+        $this->Cell(26, 5, utf8_decode("FECHA HASTA:"), 1, 0, 'L', 1);
+        $this->SetFont('Arial', '', 8);
         $this->Cell(52, 5, self::$fechaHasta, 1, 0, 'L', 1);
-        $this->SetFont('Arial','B',8);            
-        
+        $this->SetFont('Arial', 'B', 8);
+
         $this->EncabezadoDetalles();
-        
     }
 
     public function EncabezadoDetalles() {
@@ -105,36 +105,36 @@ class AnticipoResumen2 extends \FPDF_FPDF {
             
             gen_cuenta.nombre AS cuenta, 
             COUNT(car_anticipo.codigo_anticipo_pk) AS numeroAnticipos, 
-            SUM(car_anticipo.vr_total_pago) AS vrTotalPago
+            SUM(car_anticipo.vr_total) AS vrTotal
             FROM car_anticipo  
             
             LEFT JOIN gen_cuenta ON car_anticipo.codigo_cuenta_fk = gen_cuenta.codigo_cuenta_pk 
             WHERE car_anticipo.fecha >= '" . self::$fechaDesde . "' AND car_anticipo.fecha <= '" . self::$fechaHasta . "' 
             GROUP BY car_anticipo.codigo_cuenta_fk";
         $connection = self::$em->getConnection();
-        $statement = $connection->prepare($strSql);        
+        $statement = $connection->prepare($strSql);
         $statement->execute();
-        $arAnticiposResumen = $statement->fetchAll();   
+        $arAnticiposResumen = $statement->fetchAll();
         $pdf->SetX(10);
-        $pdf->SetFont('Arial', '', 7); 
+        $pdf->SetFont('Arial', '', 7);
         $total = 0;
         foreach ($arAnticiposResumen as $registro) {
             //$pdf->Cell(30, 4, $registro['tipo'], 1, 0, 'L');
             $pdf->Cell(60, 4, $registro['cuenta'], 1, 0, 'L');
             $pdf->Cell(25, 4, $registro['numeroAnticipos'], 1, 0, 'L');
-            $pdf->Cell(30, 4, number_format($registro['vrTotalPago'], 2, '.', ','), 1, 0, 'R');
-            $total += $registro['vrTotalPago'];
+            $pdf->Cell(30, 4, number_format($registro['vrTotal'], 2, '.', ','), 1, 0, 'R');
+            $total += $registro['vrTotal'];
             $pdf->Ln();
             $pdf->SetAutoPageBreak(true, 15);
         }
         $pdf->Cell(30, 4, '', 0, 0, 'L');
         $pdf->Cell(40, 4, '', 0, 0, 'L');
         $pdf->Cell(15, 4, '', 0, 0, 'L');
-        $pdf->Cell(30, 4, number_format($total, 2, '.', ','), 1, 0, 'R'); 
+        $pdf->Cell(30, 4, number_format($total, 2, '.', ','), 1, 0, 'R');
         $pdf->Ln();
         $pdf->Ln();
 
-        $header = array('COD','NRO', 'FECHA', 'CUENTA', 'NIT','CLIENTE', 'DCTO', 'AJUSTE', 'RTEICA', 'RTEIVA', 'RTEFTE', 'TOTAL', 'T. PAGO');
+        $header = array('COD', 'NRO', 'FECHA', 'CUENTA', 'NIT', 'CLIENTE', 'DCTO', 'AJUSTE', 'RTEICA', 'RTEIVA', 'RTEFTE', 'TOTAL', 'T. PAGO', 'ANU');
         $pdf->SetFillColor(236, 236, 236);
         $pdf->SetTextColor(0);
         $pdf->SetDrawColor(0, 0, 0);
@@ -142,7 +142,7 @@ class AnticipoResumen2 extends \FPDF_FPDF {
         $pdf->SetFont('', 'B', 6);
 
         //creamos la cabecera de la tabla.
-        $w = array(8,8, 14, 30, 14,45, 12, 12, 12, 12, 12, 13, 13);
+        $w = array(8, 8, 14, 20, 14, 45, 12, 12, 12, 12, 12, 13, 13, 10);
         for ($i = 0; $i < count($header); $i++)
             if ($i == 0 || $i == 1)
                 $pdf->Cell($w[$i], 4, $header[$i], 1, 0, 'L', 1);
@@ -154,17 +154,17 @@ class AnticipoResumen2 extends \FPDF_FPDF {
         $pdf->SetTextColor(0);
         $pdf->SetFont('');
         $pdf->Ln(4);
-        $dql   = "SELECT r FROM BrasaCarteraBundle:CarAnticipo r WHERE r.fecha >='" . self::$fechaDesde . "' AND r.fecha <='" . self::$fechaHasta . "'";                
+        $dql = "SELECT r FROM BrasaCarteraBundle:CarAnticipo r WHERE r.fecha >='" . self::$fechaDesde . "' AND r.fecha <='" . self::$fechaHasta . "'";
         $query = self::$em->createQuery($dql);
         $arAnticipos = $query->getResult();
         foreach ($arAnticipos as $arAnticipo) {
             //$pdf->Cell(20, 4, $arAnticipo->getAnticipoTipoRel()->getNombre(), 1, 0, 'L');                        
             $pdf->Cell(8, 4, $arAnticipo->getCodigoAnticipoPk(), 1, 0, 'L');
-            $pdf->Cell(8, 4, $arAnticipo->getNumero(), 1, 0, 'L');                        
-            $pdf->Cell(14, 4, $arAnticipo->getFecha()->format('Y/m/d'), 1, 0, 'L');                        
-            $pdf->Cell(30, 4, $arAnticipo->getCuentaRel()->getNombre(), 1, 0, 'L');                        
+            $pdf->Cell(8, 4, $arAnticipo->getNumero(), 1, 0, 'L');
+            $pdf->Cell(14, 4, $arAnticipo->getFecha()->format('Y/m/d'), 1, 0, 'L');
+            $pdf->Cell(20, 4, $arAnticipo->getCuentaRel()->getNombre(), 1, 0, 'L');
             $pdf->Cell(14, 4, $arAnticipo->getClienteRel()->getNit(), 1, 0, 'L');
-            $pdf->Cell(45, 4, substr($arAnticipo->getClienteRel()->getNombreCorto(),0,32), 1, 0, 'L');                                                
+            $pdf->Cell(45, 4, substr($arAnticipo->getClienteRel()->getNombreCorto(), 0, 32), 1, 0, 'L');
             $pdf->Cell(12, 4, number_format($arAnticipo->getVrTotalDescuento(), 0, '.', ','), 1, 0, 'R');
             $pdf->Cell(12, 4, number_format($arAnticipo->getVrTotalAjustePeso(), 0, '.', ','), 1, 0, 'R');
             $pdf->Cell(12, 4, number_format($arAnticipo->getVrTotalReteIca(), 0, '.', ','), 1, 0, 'R');
@@ -172,16 +172,23 @@ class AnticipoResumen2 extends \FPDF_FPDF {
             $pdf->Cell(12, 4, number_format($arAnticipo->getVrTotalReteFuente(), 0, '.', ','), 1, 0, 'R');
             $pdf->Cell(13, 4, number_format($arAnticipo->getVrTotal(), 0, '.', ','), 1, 0, 'R');
             $pdf->Cell(13, 4, number_format($arAnticipo->getVrTotalPago(), 0, '.', ','), 1, 0, 'R');
+            if ($arAnticipo->getEstadoAnulado() == 1) {
+                $anticipo = "SI";
+            } else {
+                $anticipo = "NO";
+            }
+            $pdf->Cell(10, 4, $anticipo, 1, 0, 'L');
             $pdf->Ln();
             $pdf->SetAutoPageBreak(true, 15);
-        }        
+        }
     }
 
     public function Footer() {
-        
-        $this->SetFont('Arial','', 8);  
+
+        $this->SetFont('Arial', '', 8);
         $this->Text(170, 290, utf8_decode('Página ') . $this->PageNo() . ' de {nb}');
-    }    
+    }
+
 }
 
 ?>
