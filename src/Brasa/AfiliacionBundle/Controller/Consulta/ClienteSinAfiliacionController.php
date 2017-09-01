3c21e73137d2180b8dd6209b3772498588ae1d92
juan <juan@juan-System-Product-Name>
2017-09-01 11:29:54 -0500

    Se agrega opcion de exportar a excel el listado de clientes sin afiliacion.

--- src/Brasa/AfiliacionBundle/Controller/Consulta/ClienteSinAfiliacionController.php
+++ src/Brasa/AfiliacionBundle/Controller/Consulta/ClienteSinAfiliacionController.php
@@ -29,6 +29,8 @@ class ClienteSinAfiliacionController extends Controller {
         $form = $this->formularioFiltro();
         $form->handleRequest($request);
         $this->lista();
+        $arContratos = $paginator->paginate($em->createQuery($this->strDqlLista), $request->query->get('page', 1), 70);
+        $arFechas = $em->getRepository('BrasaAfiliacionBundle:AfiContrato')->fechaMayor();
         if ($form->isValid()) {
             if ($form->get('BtnFiltrar')->isClicked()) {
                 $this->filtrar($form);
@@ -38,12 +40,9 @@ class ClienteSinAfiliacionController extends Controller {
             if ($form->get('BtnExcel')->isClicked()) {
                 $this->filtrar($form);
                 $this->lista();
-                $this->generarExcel();
+                $this->generarExcel($arFechas);
             }
         }
-
-        $arContratos = $paginator->paginate($em->createQuery($this->strDqlLista), $request->query->get('page', 1), 70);
-        $arFechas = $em->getRepository('BrasaAfiliacionBundle:AfiContrato')->fechaMayor();
         return $this->render('BrasaAfiliacionBundle:Consulta/Contrato:clienteSinAfiliacion.html.twig', array(
                     'arContratos' => $arContratos,
                     'arFechas' => $arFechas,
@@ -95,90 +94,75 @@ class ClienteSinAfiliacionController extends Controller {
         return $form;
     }
 
-//
-//    private function generarExcel() {
-//        ob_clean();
-//        set_time_limit(0);
-//        ini_set("memory_limit", -1);
-//        $em = $this->getDoctrine()->getManager();
-//        $session = new session;
-//        $objPHPExcel = new \PHPExcel();
-//        // Set document properties
-//        $objPHPExcel->getProperties()->setCreator("EMPRESA")
-//            ->setLastModifiedBy("EMPRESA")
-//            ->setTitle("Office 2007 XLSX Test Document")
-//            ->setSubject("Office 2007 XLSX Test Document")
-//            ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
-//            ->setKeywords("office 2007 openxml php")
-//            ->setCategory("Test result file");
-//        $objPHPExcel->getDefaultStyle()->getFont()->setName('Arial')->setSize(10); 
-//        $objPHPExcel->getActiveSheet()->getStyle('1')->getFont()->setBold(true);
-//        for($col = 'A'; $col !== 'J'; $col++) {
-//            $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);         
-//        }
-//        for($col = 'K'; $col !== 'N'; $col++) {
-//                    $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
-//                    $objPHPExcel->getActiveSheet()->getStyle($col)->getNumberFormat()->setFormatCode('#,##0');
-//        }
-//        $objPHPExcel->setActiveSheetIndex(0)
-//                    ->setCellValue('A1', 'CODIGO')
-//                    ->setCellValue('B1', 'NUMERO')
-//                    ->setCellValue('C1', 'FECHA PAGO')
-//                    ->setCellValue('D1', 'NIT')
-//                    ->setCellValue('E1', 'CLIENTE')
-//                    ->setCellValue('F1', 'ASESOR')
-//                    ->setCellValue('G1', 'DOCUMENTO')
-//                    ->setCellValue('H1', 'EMPLEADO')
-//                    ->setCellValue('I1', 'CONTRATO')
-//                    ->setCellValue('J1', 'CUENTA')
-//                    ->setCellValue('K1', 'AFILIACION')
-//                    ->setCellValue('L1', 'ADMINISTRACION')
-//                    ->setCellValue('M1', 'PAGO')
-//                    ->setCellValue('N1', 'TIPO');
-//        $i = 2;
-//                        
-//        $arDetalles = $this->strDqlLista;
-//        
-//        foreach ($arDetalles as $arDetalle) {
-//
-//            if ($arDetalle['tipo'] == 1){
-//                $tipo = "REINGRESO";
-//            } else {
-//                $tipo = "NUEVO";
-//            }
-//            $objPHPExcel->setActiveSheetIndex(0)
-//                    ->setCellValue('A' . $i, $arDetalle['codigo'])
-//                    ->setCellValue('B' . $i, $arDetalle['numero'])
-//                    ->setCellValue('C' . $i, $arDetalle['fechaPago'])
-//                    ->setCellValue('D' . $i, $arDetalle['nit'])
-//                    ->setCellValue('E' . $i, $arDetalle['cliente'])
-//                    ->setCellValue('F' . $i, $arDetalle['asesor'])
-//                    ->setCellValue('G' . $i, $arDetalle['ccEmpleado'])
-//                    ->setCellValue('H' . $i, $arDetalle['empleado'])
-//                    ->setCellValue('I' . $i, $arDetalle['contrato'])
-//                    ->setCellValue('J' . $i, $arDetalle['cuenta'])
-//                    ->setCellValue('K' . $i, $arDetalle['afiliacion'])
-//                    ->setCellValue('L' . $i, $arDetalle['administracion'])
-//                    ->setCellValue('M' . $i, $arDetalle['pago'])
-//                    ->setCellValue('N' . $i, $tipo)                    ;
-//            $i++;
-//        }
-//        
-//        $objPHPExcel->getActiveSheet()->setTitle('PagoAfiliacionesAnticipos');
-//        $objPHPExcel->setActiveSheetIndex(0);
-//        // Redirect output to a client’s web browser (Excel2007)
-//        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-//        header('Content-Disposition: attachment;filename="PagoAfiliacionesAnticipos.xlsx"');
-//        header('Cache-Control: max-age=0');
-//        // If you're serving to IE 9, then the following may be needed
-//        header('Cache-Control: max-age=1');
-//        // If you're serving to IE over SSL, then the following may be needed
-//        header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-//        header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-//        header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-//        header ('Pragma: public'); // HTTP/1.0
-//        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
-//        $objWriter->save('php://output');
-//        exit;
-//    }
+    private function generarExcel($arFechas) {
+        ob_clean();
+        set_time_limit(0);
+        ini_set("memory_limit", -1);
+        $em = $this->getDoctrine()->getManager();
+        $session = new session;
+        $objPHPExcel = new \PHPExcel();
+        // Set document properties
+        $objPHPExcel->getProperties()->setCreator("EMPRESA")
+                ->setLastModifiedBy("EMPRESA")
+                ->setTitle("Office 2007 XLSX Test Document")
+                ->setSubject("Office 2007 XLSX Test Document")
+                ->setDescription("Test document for Office 2007 XLSX, generated using PHP classes.")
+                ->setKeywords("office 2007 openxml php")
+                ->setCategory("Test result file");
+        $objPHPExcel->getDefaultStyle()->getFont()->setName('Arial')->setSize(10);
+        $objPHPExcel->getActiveSheet()->getStyle('1')->getFont()->setBold(true);
+        for ($col = 'A'; $col !== 'J'; $col++) {
+            $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
+        }
+        for ($col = 'K'; $col !== 'N'; $col++) {
+            $objPHPExcel->getActiveSheet()->getColumnDimension($col)->setAutoSize(true);
+            $objPHPExcel->getActiveSheet()->getStyle($col)->getNumberFormat()->setFormatCode('#,##0');
+        }
+        $objPHPExcel->setActiveSheetIndex(0)
+                ->setCellValue('A1', 'CODIGO')
+                ->setCellValue('B1', 'CLIENTE')
+                ->setCellValue('C1', 'TELEFONO')
+                ->setCellValue('D1', 'CORREO')
+                ->setCellValue('E1', 'VALOR')
+                ->setCellValue('F1', 'FECHA');
+        $i = 2;
+
+        $query = $em->createQuery($this->strDqlLista);
+        $arClientes = new \Brasa\AfiliacionBundle\Entity\AfiCliente();
+        $arClientes = $query->getResult();
+        foreach ($arClientes as $arCliente) {
+            $fecha = "";
+            foreach ($arFechas as $arFecha) {
+                if ($arFecha[0] == $arCliente->getCodigoClientePk()) {
+                    $fecha = $arFecha[1];
+                }
+            }
+            $objPHPExcel->setActiveSheetIndex(0)
+                    ->setCellValue('A' . $i, $arCliente->getCodigoClientePk())
+                    ->setCellValue('B' . $i, $arCliente->getNombreCorto())
+                    ->setCellValue('C' . $i, $arCliente->getTelefono())
+                    ->setCellValue('D' . $i, $arCliente->getEmail())
+                    ->setCellValue('E' . $i, $arCliente->getAfiliacion())
+                    ->setCellValue('F' . $i, $fecha);
+            $i++;
+        }
+
+        $objPHPExcel->getActiveSheet()->setTitle('ClientesSinAfiliacion');
+        $objPHPExcel->setActiveSheetIndex(0);
+        // Redirect output to a client’s web browser (Excel2007)
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="PagoAfiliacionesAnticipos.xlsx"');
+        header('Cache-Control: max-age=0');
+        // If you're serving to IE 9, then the following may be needed
+        header('Cache-Control: max-age=1');
+        // If you're serving to IE over SSL, then the following may be needed
+        header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+        header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+        header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+        header('Pragma: public'); // HTTP/1.0
+        $objWriter = new \PHPExcel_Writer_Excel2007($objPHPExcel);
+        $objWriter->save('php://output');
+        exit;
+    }
+
 }
