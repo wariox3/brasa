6adc10f8e852603c7ae386f2e4d368a7ee9553ef
pc--25 <pc--25@pc25-H81M-H>
2017-10-31 15:53:59 -0500

    No generar interes mora

--- src/Brasa/AfiliacionBundle/Repository/AfiPeriodoRepository.php
+++ src/Brasa/AfiliacionBundle/Repository/AfiPeriodoRepository.php
@@ -723,7 +723,7 @@ class AfiPeriodoRepository extends EntityRepository {
                 if ($arPeriodo->getInteresMora() == 0) {
                     $valorTotal = $arPeriodo->getTotal();
                     $valorSubtotal = $arPeriodo->getSubtotal();
-                    $porcentajeInteres = 0.09;
+                    $porcentajeInteres = 0;
                     $valorInteresMora = $valorSubtotal * $porcentajeInteres / 100 * 13;
                     $arPeriodo->setTotalAnterior($valorTotal);
                     $arPeriodo->setInteresMora($valorInteresMora);
@@ -733,7 +733,7 @@ class AfiPeriodoRepository extends EntityRepository {
                 } else {
                     $valorTotal = $arPeriodo->getTotalAnterior();
                     $valorSubtotal = $arPeriodo->getSubtotalAnterior();
-                    $porcentajeInteres = 0.09;
+                    $porcentajeInteres = 0;
                     $valorInteresMora = $valorTotal * $porcentajeInteres / 100 * 13;
                     $arPeriodo->setInteresMora($valorInteresMora);
                     $arPeriodo->setSubtotal($valorSubtotal + $valorInteresMora);
